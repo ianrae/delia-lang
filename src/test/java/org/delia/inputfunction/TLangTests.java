387acbbdaa4490b3b699ca713f2d0aa0d976b3fb
Ian Rae <ianrae88@gmail.com>
2020-04-29 13:28:50 -0400

    condition

--- src/test/java/org/delia/inputfunction/TLangTests.java
+++ src/test/java/org/delia/inputfunction/TLangTests.java
@@ -17,6 +17,7 @@ import org.delia.dao.DeliaDao;
 import org.delia.db.DBInterface;
 import org.delia.db.DBType;
 import org.delia.db.memdb.MemDBInterface;
+import org.delia.db.memdb.filter.OP;
 import org.delia.error.DeliaError;
 import org.delia.log.LogLevel;
 import org.delia.type.DTypeRegistry;
@@ -35,7 +36,7 @@ public class TLangTests  extends NewBDDBase {
 		public ScalarValueBuilder builder;
 	}
 	public interface TLangStatement {
-		boolean evalCondition();
+		boolean evalCondition(DValue dval);
 		void execute(DValue value, TLangResult result, TLangContext ctx);
 	}
 	
@@ -43,21 +44,79 @@ public class TLangTests  extends NewBDDBase {
 		List<TLangStatement> statements = new ArrayList<>();
 	}
 	
-	public static class IfStatement implements TLangStatement {
-		@Override
-		public void execute(DValue value, TLangResult result, TLangContext ctx) {
+	
+	public interface Condition {
+		boolean eval(DValue dval);
+	}
+	public static class BasicCondition implements Condition {
+		public boolean bb;
+		
+		public BasicCondition(boolean b) {
+			this.bb = b;
 		}
 		@Override
-		public boolean evalCondition() {
-			return false;
+		public boolean eval(DValue dval) {
+			return bb;
+		}
+	}
+	
+	public static class IsMissingCondition implements Condition {
+		
+		public IsMissingCondition() {
+		}
+		@Override
+		public boolean eval(DValue dval) {
+			return dval == null || dval.asString().isEmpty();
+		}
+	}
+	public static class OpCondition implements Condition {
+		public OP op;
+		
+		public OpCondition(OP op) {
+		}
+		@Override
+		public boolean eval(DValue dval) {
+			return dval == null || dval.asString().isEmpty();
+		}
+	}
+	
+	
+	public static class IfStatement implements TLangStatement {
+		public Condition cond;
+		
+		public IfStatement(Condition cond) {
+			this.cond = cond;
+		}
+		@Override
+		public void execute(DValue value, TLangResult result, TLangContext ctx) {
+			result.val = value;
+		}
+		@Override
+		public boolean evalCondition(DValue dval) {
+			return cond.eval(dval);
+		}
+	}
+	public static class ElseIfStatement implements TLangStatement {
+		public Condition cond;
+		public ElseIfStatement(Condition cond) {
+			this.cond = cond;
+		}
+		@Override
+		public void execute(DValue value, TLangResult result, TLangContext ctx) {
+			result.val = value;
+		}
+		@Override
+		public boolean evalCondition(DValue dval) {
+			return cond.eval(dval);
 		}
 	}
 	public static class EndIfStatement implements TLangStatement {
 		@Override
 		public void execute(DValue value, TLangResult result, TLangContext ctx) {
+			result.val = value;
 		}
 		@Override
-		public boolean evalCondition() {
+		public boolean evalCondition(DValue dval) {
 			return true;
 		}
 	}
@@ -81,7 +140,7 @@ public class TLangTests  extends NewBDDBase {
 			int ipIndex;
 			for(ipIndex = 0; ipIndex < program.statements.size(); ipIndex++) {
 				TLangStatement statement = program.statements.get(ipIndex);
-				if (statement.evalCondition()) {
+				if (statement.evalCondition(dval)) {
 					TLangContext ctx = new TLangContext();
 					ctx.builder = scalarBuilder;
 					res.ok = true;
@@ -98,7 +157,6 @@ public class TLangTests  extends NewBDDBase {
 				}
 			}
 			
-			
 			TLangResult result = res;
 			result.ok = true;
 			result.val = dval;
@@ -108,7 +166,7 @@ public class TLangTests  extends NewBDDBase {
 		private int findNext(TLangProgram program, int ipIndexCurrent) {
 			for(int ipIndex = ipIndexCurrent + 1; ipIndex < program.statements.size(); ipIndex++) {
 				TLangStatement statement = program.statements.get(ipIndex);
-				if (statement instanceof EndIfStatement) {
+				if (statement instanceof EndIfStatement || statement instanceof ElseIfStatement) {
 					return ipIndex;
 				}
 			}
@@ -120,7 +178,7 @@ public class TLangTests  extends NewBDDBase {
 		@Override
 		public abstract void execute(DValue value, TLangResult result, TLangContext ctx);
 		@Override
-		public boolean evalCondition() {
+		public boolean evalCondition(DValue dval) {
 			return true;
 		}
 	}
@@ -131,7 +189,6 @@ public class TLangTests  extends NewBDDBase {
 			s = s.toUpperCase();
 			result.val = ctx.builder.buildString(s);
 		}
-
 	}
 	public static class AddXStatement extends TLangStatementBase {
 		@Override
@@ -141,11 +198,21 @@ public class TLangTests  extends NewBDDBase {
 			result.val = ctx.builder.buildString(s);
 		}
 	}
+	public static class ValueStatement extends TLangStatementBase {
+		private DValue dval;
+		public ValueStatement(DValue dval) {
+			this.dval = dval;
+		}
+		@Override
+		public void execute(DValue value, TLangResult result, TLangContext ctx) {
+			result.val = dval;
+		}
+
+	}
 	
 	@Test
-	public void test2() {
+	public void test1() {
 		TLangRunner inFuncRunner = createXConv();
-		
 		TLangProgram prog = createProgram();
 		
 		DValue initialValue = builder.buildString("abc");
@@ -155,11 +222,80 @@ public class TLangTests  extends NewBDDBase {
 		DValue dval = (DValue) res.val;
 		assertEquals("ABCX", dval.asString());
 	}
+	
+	@Test
+	public void test2() {
+		TLangRunner inFuncRunner = createXConv();
+		TLangProgram prog = createProgram2(false);
+		
+		DValue initialValue = builder.buildString("abc");
+		TLangResult res = inFuncRunner.execute(prog, initialValue);
+
+		assertEquals(true, res.ok);
+		DValue dval = (DValue) res.val;
+		assertEquals("abcX", dval.asString());
+	}
+	@Test
+	public void test2a() {
+		TLangRunner inFuncRunner = createXConv();
+		TLangProgram prog = createProgram2(true);
+		
+		DValue initialValue = builder.buildString("abc");
+		TLangResult res = inFuncRunner.execute(prog, initialValue);
+
+		assertEquals(true, res.ok);
+		DValue dval = (DValue) res.val;
+		assertEquals("ABCX", dval.asString());
+	}
+	@Test
+	public void test3() {
+		TLangRunner inFuncRunner = createXConv();
+		TLangProgram prog = createProgram3(false);
+		
+		DValue initialValue = builder.buildString("");
+		TLangResult res = inFuncRunner.execute(prog, initialValue);
+
+		assertEquals(true, res.ok);
+		DValue dval = (DValue) res.val;
+		assertEquals("Z", dval.asString());
+	}
+	@Test
+	public void test3a() {
+		TLangRunner inFuncRunner = createXConv();
+		TLangProgram prog = createProgram3(true);
+		
+		DValue initialValue = builder.buildString("");
+		TLangResult res = inFuncRunner.execute(prog, initialValue);
+
+		assertEquals(true, res.ok);
+		DValue dval = (DValue) res.val;
+		assertEquals("ZX", dval.asString());
+	}
 
 	private TLangProgram createProgram() {
 		TLangProgram prog = new TLangProgram();
 		prog.statements.add(new ToUpperStatement());
 		prog.statements.add(new AddXStatement());
+		return prog;
+	}
+	private TLangProgram createProgram2(boolean bb) {
+		TLangProgram prog = new TLangProgram();
+		prog.statements.add(new IfStatement(new BasicCondition(bb)));
+		prog.statements.add(new ToUpperStatement());
+		prog.statements.add(new EndIfStatement());
+		prog.statements.add(new AddXStatement());
+		return prog;
+	}
+	private TLangProgram createProgram3(boolean bb) {
+		TLangProgram prog = new TLangProgram();
+		prog.statements.add(new IfStatement(new IsMissingCondition()));
+		
+		DValue x = builder.buildString("Z");
+		prog.statements.add(new ValueStatement(x));
+		prog.statements.add(new EndIfStatement());
+		if (bb) {
+			prog.statements.add(new AddXStatement());
+		}
 		return prog;
 	}
 
