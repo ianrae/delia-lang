fe6cde01922a3360934208b52ffa9505b9214be4
Ian Rae <ianrae88@gmail.com>
2020-05-07 16:11:20 -0400

    delete old qff functions

--- src/main/java/org/delia/queryresponse/QueryResponseFunctionFactory.java
+++ src/main/java/org/delia/queryresponse/QueryResponseFunctionFactory.java
@@ -2,17 +2,6 @@ package org.delia.queryresponse;
 
 import org.delia.core.FactoryService;
 import org.delia.core.ServiceBase;
-import org.delia.queryresponse.function.CountFunction;
-import org.delia.queryresponse.function.DistinctFunction;
-import org.delia.queryresponse.function.ExistsFunction;
-import org.delia.queryresponse.function.FKsFunction;
-import org.delia.queryresponse.function.FetchFunction;
-import org.delia.queryresponse.function.FirstFunction;
-import org.delia.queryresponse.function.LimitFunction;
-import org.delia.queryresponse.function.MaxFunction;
-import org.delia.queryresponse.function.MinFunction;
-import org.delia.queryresponse.function.OffsetFunction;
-import org.delia.queryresponse.function.OrderByFunction;
 import org.delia.runner.FetchRunner;
 import org.delia.type.DTypeRegistry;
 
@@ -25,39 +14,39 @@ public class QueryResponseFunctionFactory extends ServiceBase {
 	}
 
 	public QueryResponseFunction create(String fnName, DTypeRegistry registry) {
-		switch(fnName) {
-		case "min":
-			return new MinFunction(registry);
-		case "max":
-			return new MaxFunction(registry);
-		case "count":
-			return new CountFunction(registry);
-		case "distinct":
-			return new DistinctFunction(registry);
-		case "exist":
-			return new ExistsFunction(registry);
-		case "fetch":
-			return new FetchFunction(registry, fetchRunner);
-		case "fks":
-			return new FKsFunction(registry, factorySvc.getConfigureService());
-		case "orderBy":
-			return new OrderByFunction(registry);
-		case "limit":
-			return new LimitFunction(registry);
-		case "offset":
-			return new OffsetFunction(registry);
-		case "first":
-			return new FirstFunction(registry, true, false);
-		case "last":
-			return new FirstFunction(registry, false, false);
-		case "ith":
-			return new FirstFunction(registry, false, true);
-		default:
-		{
-			String msg = String.format("unknown fn: %s", fnName);
-			et.add("unknown-query-function", msg);
+//		switch(fnName) {
+//		case "min":
+//			return new MinFunction(registry);
+//		case "max":
+//			return new MaxFunction(registry);
+//		case "count":
+//			return new CountFunction(registry);
+//		case "distinct":
+//			return new DistinctFunction(registry);
+//		case "exist":
+//			return new ExistsFunction(registry);
+//		case "fetch":
+//			return new FetchFunction(registry, fetchRunner);
+//		case "fks":
+//			return new FKsFunction(registry, factorySvc.getConfigureService());
+//		case "orderBy":
+//			return new OrderByFunction(registry);
+//		case "limit":
+//			return new LimitFunction(registry);
+//		case "offset":
+//			return new OffsetFunction(registry);
+//		case "first":
+//			return new FirstFunction(registry, true, false);
+//		case "last":
+//			return new FirstFunction(registry, false, false);
+//		case "ith":
+//			return new FirstFunction(registry, false, true);
+//		default:
+//		{
+//			String msg = String.format("unknown fn: %s", fnName);
+//			et.add("unknown-query-function", msg);
 			return null;
-		}
-		}
+//		}
+//		}
 	}
 }
