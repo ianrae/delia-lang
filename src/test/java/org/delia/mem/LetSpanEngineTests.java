8f9d720660fd8f4f9bff064d7f41b7acf543456b
Ian Rae <ianrae88@gmail.com>
2020-05-07 10:11:02 -0400

    m

--- src/test/java/org/delia/mem/LetSpanEngineTests.java
+++ src/test/java/org/delia/mem/LetSpanEngineTests.java
@@ -1,40 +1,352 @@
 package org.delia.mem;
 
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
+
+import org.apache.commons.collections.CollectionUtils;
 import org.delia.api.Delia;
 import org.delia.bdd.NewBDDBase;
+import org.delia.builder.ConnectionBuilder;
 import org.delia.builder.ConnectionInfo;
 import org.delia.builder.DeliaBuilder;
+import org.delia.compiler.ast.Exp;
+import org.delia.compiler.ast.IntegerExp;
 import org.delia.compiler.ast.QueryExp;
+import org.delia.compiler.ast.QueryFieldExp;
+import org.delia.compiler.ast.QueryFuncExp;
 import org.delia.core.FactoryService;
 import org.delia.core.ServiceBase;
-import org.delia.builder.ConnectionBuilder;
 import org.delia.dao.DeliaDao;
 import org.delia.db.DBInterface;
 import org.delia.db.DBType;
 import org.delia.db.memdb.MemDBInterface;
-import org.delia.log.Log;
-import org.delia.log.StandardLogFactory;
-import org.delia.runner.DeliaException;
+import org.delia.queryresponse.QueryFuncContext;
+import org.delia.queryresponse.QueryResponseFunctionBase;
+import org.delia.runner.FetchRunner;
 import org.delia.runner.QueryResponse;
 import org.delia.runner.ResultValue;
+import org.delia.type.DStructType;
+import org.delia.type.DType;
+import org.delia.type.DTypeRegistry;
 import org.delia.type.DValue;
+import org.delia.util.DValueHelper;
+import org.delia.util.DeliaExceptionHelper;
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class LetSpanEngineTests extends NewBDDBase {
+	
+	public interface ZQueryResponseFunction {
+		public QueryResponse process(QueryFuncExp qfe, QueryResponse qresp, QueryFuncContext ctx);
+	}	
+	public static abstract class ZQueryResponseFunctionBase implements ZQueryResponseFunction {
+
+		protected DTypeRegistry registry;
+		
+		public ZQueryResponseFunctionBase(DTypeRegistry registry) {
+			this.registry = registry;
+		}
+		public abstract QueryResponse process(QueryFuncExp qfe, QueryResponse qresp, QueryFuncContext ctx);
+		
+		protected String getStringArg(QueryFuncExp qfe, QueryFuncContext ctx) {
+			String s = qfe.argL.get(0).strValue();
+			return s;
+		}
+		protected int getIntArg(QueryFuncExp qfe, QueryFuncContext ctx) {
+			Exp arg = qfe.argL.get(0);
+			IntegerExp nexp = (IntegerExp) arg;
+			return nexp.val;
+		}
+		
+		protected void ensureFieldExists(QueryResponse qresp, String fnName, String fieldName) {
+			if (CollectionUtils.isEmpty(qresp.dvalList)) {
+				return;
+			}
+			DValue dval = qresp.dvalList.get(0);
+			DValueHelper.throwIfFieldNotExist(fnName, fieldName, dval);
+		}
+		
+	}	
+	public static class ZOrderByFunction extends ZQueryResponseFunctionBase {
+		public ZOrderByFunction(DTypeRegistry registry) {
+			super(registry);
+		}
+
+		@Override
+		public QueryResponse process(QueryFuncExp qfe, QueryResponse qresp, QueryFuncContext ctx) {
+			List<DValue> dvalList = qresp.dvalList;
+			if (dvalList == null || dvalList.size() <= 1) {
+				return qresp; //nothing to sort
+			}
+			
+			String fieldName = getStringArg(qfe, ctx); //"wid";
+			
+			TreeMap<Object,List<DValue>> map = new TreeMap<>();
+			List<DValue> nulllist = new ArrayList<>();
+
+			ensureFieldExists(qresp, "orderBy", fieldName);
+			for(DValue dval: dvalList) {
+				DValue inner = dval.asStruct().getField(fieldName);
+				
+				if (inner == null) {
+					nulllist.add(dval);
+				} else {
+					List<DValue> valuelist = map.get(inner.getObject());
+					if (valuelist == null) {
+						valuelist = new ArrayList<>();
+					}
+					valuelist.add(inner);
+					map.put(inner.getObject(), valuelist);
+				}
+			}
+			
+			List<DValue> newlist = new ArrayList<>();
+			for(Object key: map.keySet()) {
+				List<DValue> valuelist = map.get(key);
+				newlist.addAll(valuelist);
+			}
+			
+			//add null values
+			boolean asc = isAsc(qfe, ctx);
+			if (asc) {
+				nulllist.addAll(newlist);
+				newlist = nulllist;
+			} else {
+				newlist.addAll(nulllist);
+			}
+			
+			if (! asc) {
+				Collections.reverse(newlist);
+			}
+			
+			qresp.dvalList = newlist;
+			return qresp;
+		}
+
+		private boolean isAsc(QueryFuncExp qfe, QueryFuncContext ctx) {
+			if (qfe.argL.size() == 2) {
+				Exp arg = qfe.argL.get(1);
+				return arg.strValue().equals("asc");
+			}
+			return true;
+		}
+	}	
+	public static class OffsetFunction extends ZQueryResponseFunctionBase {
+		public OffsetFunction(DTypeRegistry registry) {
+			super(registry);
+		}
+
+		@Override
+		public QueryResponse process(QueryFuncExp qfe, QueryResponse qresp, QueryFuncContext ctx) {
+			
+			List<DValue> dvalList = qresp.dvalList;
+			if (dvalList == null || dvalList.size() <= 1) {
+				return qresp; //nothing to sort
+			}
+			
+			int offset = getIntArg(qfe, ctx);
+			ctx.currentOffset = offset;
+			
+			doLimitAndOffset(ctx, qresp);
+			return qresp;
+		}
+		
+		protected boolean canExecuteInGivenOrder(QueryFuncContext ctx) {
+			//Flight[true].offset(1).limit(2) is ok
+			int pos1 = ctx.pendingTrail.getTrail().indexOf("offset");
+			int pos2 = ctx.pendingTrail.getTrail().indexOf("limit");
+			if (pos1 < 0 || pos2 < 0) {
+				return true;
+			} else if (pos1 < pos2) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		protected void doLimitAndOffset(QueryFuncContext ctx, QueryResponse qresp) {
+			int offset = ctx.currentOffset;
+			int pgSize = ctx.currentPgSize;
+			
+			ctx.currentOffset = 0; //reset
+			
+			if (ctx.offsetLimitDirtyFlag) {
+				ctx.offsetLimitDirtyFlag = true;
+				List<DValue> newlist = new ArrayList<>();
+				int i = 0;
+				List<DValue> dvalList = ctx.getDValList();
+				for(DValue dval: dvalList) {
+					if (offset > 0) {
+						offset--;
+						continue;
+					}
+					
+					if (i == pgSize) {
+						break;
+					}
+					newlist.add(dval);
+					i++;
+				}
+				
+				qresp.dvalList = newlist;
+			}
+		}
+	}
+	public static class ZQueryResponseFunctionFactory extends ServiceBase {
+		private FetchRunner fetchRunner;
+
+		public ZQueryResponseFunctionFactory(FactoryService factorySvc, FetchRunner fetchRunner) {
+			super(factorySvc);
+			this.fetchRunner = fetchRunner;
+		}
+
+		public ZQueryResponseFunction create(String fnName, DTypeRegistry registry) {
+			switch(fnName) {
+//			case "min":
+//				return new MinFunction(registry);
+//			case "max":
+//				return new MaxFunction(registry);
+//			case "count":
+//				return new CountFunction(registry);
+//			case "distinct":
+//				return new DistinctFunction(registry);
+//			case "exist":
+//				return new ExistsFunction(registry);
+//			case "fetch":
+//				return new FetchFunction(registry, fetchRunner);
+//			case "fks":
+//				return new FKsFunction(registry, factorySvc.getConfigureService());
+			case "orderBy":
+				return new ZOrderByFunction(registry);
+//			case "limit":
+//				return new LimitFunction(registry);
+//			case "offset":
+//				return new OffsetFunction(registry);
+//			case "first":
+//				return new FirstFunction(registry, true, false);
+//			case "last":
+//				return new FirstFunction(registry, false, false);
+//			case "ith":
+//				return new FirstFunction(registry, false, true);
+			default:
+			{
+				String msg = String.format("unknown fn: %s", fnName);
+				et.add("unknown-query-function", msg);
+				return null;
+			}
+			}
+		}
+
+		public boolean isPass1Function(String fnName) {
+			String[] arPass1Fn = { "orderBy", "limit", "offset" };
+			List<String> list = Arrays.asList(arPass1Fn);
+			return list.contains(fnName);
+		}
+	}	
+	
+	
+	public static class LetSpan {
+		public DStructType structType;
+		public List<QueryFuncExp> qfeL;
+		public QueryResponse qresp;
+		
+		public LetSpan(DType dtype) {
+			this.structType = (DStructType) dtype;
+		}
+	}
 
 	public static class LetSpanEngine extends ServiceBase {
 
-		public LetSpanEngine(FactoryService factorySvc) {
+		private DTypeRegistry registry;
+
+
+		public LetSpanEngine(FactoryService factorySvc, DTypeRegistry registry) {
 			super(factorySvc);
+			this.registry = registry;
 		}
 
 		
-		public QueryResponse process(QueryExp queryExp, QueryResponse qresp0) {
+		public QueryResponse process(QueryExp queryExp, QueryResponse qrespInitial) {
+			List<LetSpan> spanL = buildSpans(queryExp);
+			
+			//execute span
+			QueryResponse qresp = qrespInitial;
+			for(LetSpan span: spanL) {
+				span.qresp = qresp;
+				qresp = executeSpan(span);
+			}
+			
+			return qresp;
+		}
+
+
+		private QueryResponse executeSpan(LetSpan span) {
+			FetchRunner fetchRunner = null;
+			ZQueryResponseFunctionFactory fnFactory = new ZQueryResponseFunctionFactory(factorySvc, fetchRunner);
+			
+			QueryResponse qresp = span.qresp;
+			for(int i = 0; i < span.qfeL.size(); i++) {
+				QueryFuncExp qfexp = span.qfeL.get(i);
+				if (qfexp instanceof QueryFieldExp) {
+				} else {
+					String fnName = qfexp.funcName;
+					log.log("qfn: " + fnName);
+					if (fnFactory.isPass1Function(fnName)) {
+						ZQueryResponseFunction func = fnFactory.create(fnName, registry);
+						if (func == null) {
+							DeliaExceptionHelper.throwError("unknown-let-function", "Unknown let function '%s'", fnName);
+						} else {
+							QueryFuncContext ctx = new QueryFuncContext();
+							qresp = func.process(qfexp, qresp, ctx);
+						}
+
+					}
+				}
+			}
+			return qresp;
+		}
+
+
+		private List<LetSpan> buildSpans(QueryExp queryExp) {
+			List<LetSpan> spanL = new ArrayList<>();
+			LetSpan span = new LetSpan(registry.getType(queryExp.typeName));
+			for(int i = 0; i < queryExp.qfelist.size(); i++) {
+				QueryFuncExp qfexp = queryExp.qfelist.get(i);
+				
+				LetSpan possibleNewSpan = endsSpan(span, qfexp);
+				if (possibleNewSpan != null) {
+					spanL.add(span);
+					span = possibleNewSpan;
+				} else {
+					span.qfeL.add(qfexp);
+				}
+			}
+			
+			if (! span.qfeL.isEmpty()) {
+				spanL.add(span);
+			}
+			
+			return spanL;
+		}
+
+
+		private LetSpan endsSpan(LetSpan span, QueryFuncExp qfexp) {
+			if (qfexp instanceof QueryFieldExp) {
+				QueryFieldExp qff = (QueryFieldExp) qfexp;
+				String fieldName = qff.funcName;
+				DType fieldType = DValueHelper.findFieldType(span.structType, fieldName);
+				if (fieldType.isStructShape()) {
+					LetSpan newSpan = new LetSpan(fieldType);
+					return newSpan;
+				}
+			}
 			return null;
 		}
 	}
