bb0e418675a072e3298d639200f12de212fd5d39
Ian Rae <ianrae88@gmail.com>
2020-05-08 16:38:01 -0400

    .addr

--- src/test/java/org/delia/mem/HLSTests.java
+++ src/test/java/org/delia/mem/HLSTests.java
@@ -327,6 +327,7 @@ public class HLSTests extends NewBDDBase {
 			int i = 0;
 			for(LetSpan span: spanL) {
 				if (i < iStart) {
+					i++;
 					continue;
 				} else if (i > iStart) {
 					break;
@@ -359,6 +360,7 @@ public class HLSTests extends NewBDDBase {
 			int i = 0;
 			for(LetSpan span: spanL) {
 				if (i < iStart) {
+					i++;
 					continue;
 				} else if (i > iStart) {
 					break;
@@ -384,6 +386,7 @@ public class HLSTests extends NewBDDBase {
 			int i = 0;
 			for(LetSpan span: spanL) {
 				if (i < iStart) {
+					i++;
 					continue;
 				} else if (i > iStart) {
 					break;
@@ -419,6 +422,7 @@ public class HLSTests extends NewBDDBase {
 			int i = 0;
 			for(LetSpan span: spanL) {
 				if (i < iStart) {
+					i++;
 					continue;
 				} else if (i > iStart) {
 					break;
@@ -438,6 +442,7 @@ public class HLSTests extends NewBDDBase {
 			int i = 0;
 			for(LetSpan span: spanL) {
 				if (i < iStart) {
+					i++;
 					continue;
 				} else if (i > iStart) {
 					break;
@@ -458,6 +463,7 @@ public class HLSTests extends NewBDDBase {
 			int i = 0;
 			for(LetSpan span: spanL) {
 				if (i < iStart) {
+					i++;
 					continue;
 				} else if (i > iStart) {
 					break;
@@ -480,6 +486,7 @@ public class HLSTests extends NewBDDBase {
 			int i = 0;
 			for(LetSpan span: spanL) {
 				if (i < iStart) {
+					i++;
 					continue;
 				} else if (i > iStart) {
 					break;
@@ -527,7 +534,7 @@ public class HLSTests extends NewBDDBase {
 				if (qfe instanceof QueryFieldExp) {
 					String fieldName = qfe.funcName;
 					TypePair pair = DValueHelper.findField(currentType, fieldName);
-					if (!pair.type.isStructShape()) {
+					if (pair != null && !pair.type.isStructShape()) {
 						return pair;
 					}
 				}
@@ -539,7 +546,7 @@ public class HLSTests extends NewBDDBase {
 				if (qfe instanceof QueryFieldExp) {
 					String fieldName = qfe.funcName;
 					TypePair pair = DValueHelper.findField(currentType, fieldName);
-					if (pair.type.isStructShape()) {
+					if (pair != null && pair.type.isStructShape()) {
 						return pair;
 					}
 				}
@@ -585,21 +592,27 @@ public class HLSTests extends NewBDDBase {
 		chk("let x = Customer[true].x.fetch('addr')", "{Customer->int,MT:Customer,[true],F:x,()}");
 		
 		chk("let x = Customer[true].x.fks()", "{Customer->int,MT:Customer,[true],F:x,(),SUB:true}");
-
-		
-//		chk("let x = Flight[55].field1", "{Flight->int,MT:Flight,[55],F:field1,()}");
-////		chk("let x = Flight[55].field1", "{Flight->Flight,MT:Flight,FIL:Flight[55],[]}");
-//		chk("let x = Flight[55].field1.min()", "{Flight->int,MT:Flight,[55],F:field1,(min)}");
-//		chk("let x = Flight[55].field1.orderBy('min')", "{Flight->int,MT:Flight,[55],F:field1,(),OLO:min,null,null}");
-//		chk("let x = Flight[55].field1.orderBy('min').offset(3)", "{Flight->int,MT:Flight,[55],F:field1,(),OLO:min,null,3}");
-//		chk("let x = Flight[55].field1.orderBy('min').offset(3).limit(5)", "{Flight->int,MT:Flight,[55],F:field1,(),OLO:min,5,3}");
-//		
-//		chk("let x = Flight[55].count()", "{Flight->long,MT:Flight,[55],(count)}");
-//		chk("let x = Flight[55].field1.count()", "{Flight->long,MT:Flight,[55],F:field1,(count)}");
-//		chk("let x = Flight[55].field1.distinct()", "{Flight->int,MT:Flight,[55],F:field1,(distinct)}");
-//		chk("let x = Flight[55].field1.exists()", "{Flight->boolean,MT:Flight,[55],F:field1,(exists)}");
-//		chk("let x = Flight[55].first()", "{Flight->Flight,MT:Flight,[55],(first)}");
 	}
+	
+	@Test
+	public void testOneRelation() {
+		useCustomerSrc = true;
+		chk("let x = Customer[true].addr", "{Address->Address,MT:Address,[true],R:addr,()}");
+		
+		
+		
+		chk("let x = Customer[true].fks()", "{Customer->Customer,MT:Customer,[true],(),SUB:true}");
+		chk("let x = Customer[true].fetch('addr')", "{Customer->Customer,MT:Customer,[true],(),SUB:false,addr}");
+		
+		chk("let x = Customer[true].fetch('addr').first()", "{Customer->Customer,MT:Customer,[true],(first),SUB:false,addr}");
+		chk("let x = Customer[true].fetch('addr').orderBy('id')", "{Customer->Customer,MT:Customer,[true],(),SUB:false,addr,OLO:id,null,null}");
+
+		//this one doesn't need to do fetch since just getting x
+		chk("let x = Customer[true].x.fetch('addr')", "{Customer->int,MT:Customer,[true],F:x,()}");
+		
+		chk("let x = Customer[true].x.fks()", "{Customer->int,MT:Customer,[true],F:x,(),SUB:true}");
+	}
+	
 	
 	@Test
 	public void testDebug() {
@@ -607,7 +620,9 @@ public class HLSTests extends NewBDDBase {
 		
 		useCustomerSrc = true;
 //		chk("let x = Customer[true].fks()", "{Customer->Customer,MT:Customer,[true],(fks),SUB:true}");
-		chk("let x = Customer[true].x.fks()", "{Customer->int,MT:Customer,[true],F:x,(),SUB:true}");
+//		chk("let x = Customer[true].x.fks()", "{Customer->int,MT:Customer,[true],F:x,(),SUB:true}");
+//		chk("let x = Customer[true].addr.fks()", "{Address->Address,MT:Address,[true],R:addr,(),SUB:true}");
+		chk("let x = Customer[true].fks().addr", "{Address->Address,MT:Address,[true],R:addr,(),SUB:true}");
 		
 	}
 	
@@ -620,11 +635,13 @@ public class HLSTests extends NewBDDBase {
 		
 		HLSEngine hlsEngine = new HLSEngine(delia.getFactoryService(), session.getExecutionContext().registry);
 		HLSQueryStatement hls = hlsEngine.generateStatement(queryExp, spanL);
-		assertEquals(1, hls.hlspanL.size());
-		HLSQuerySpan hlspan = hls.hlspanL.get(0);
-//		assertEquals("Flight", hlspan.mtEl.getTypeName());
+		
+		for(HLSQuerySpan hlspan: hls.hlspanL) {
+			String hlstr = hlspan.toString();
+			log.log(hlstr);
+		}
+//		assertEquals(1, hls.hlspanL.size());
 		String hlstr = hls.toString();
-		log.log(hlstr);
 		assertEquals(expected, hlstr);
 	}
 
