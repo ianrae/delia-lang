07ec01dacc183f400e506055663d84eaf9362e05
Ian Rae <ianrae88@gmail.com>
2020-05-14 07:25:07 -0400

    refac

---
+++ src/main/java/org/delia/assoc/CreateNewDatIdVisitor.java
@@ -0,0 +1,72 @@
+package org.delia.assoc;
+
+import org.delia.core.FactoryService;
+import org.delia.db.InsertContext;
+import org.delia.db.schema.SchemaMigrator;
+import org.delia.log.Log;
+import org.delia.rule.rules.RelationRuleBase;
+import org.delia.type.DStructType;
+import org.delia.type.DTypeRegistry;
+import org.delia.type.DValue;
+import org.delia.valuebuilder.ScalarValueBuilder;
+import org.delia.valuebuilder.StructValueBuilder;
+
+public class CreateNewDatIdVisitor implements ManyToManyVisitor {
+	private FactoryService factorySvc;
+	private DTypeRegistry registry;
+	private SchemaMigrator schemaMigrator;
+	private Log log;
+
+	public CreateNewDatIdVisitor(FactoryService factorySvc, SchemaMigrator schemaMigrator, DTypeRegistry registry, Log log) {
+		this.factorySvc = factorySvc;
+		this.schemaMigrator = schemaMigrator;
+		this.registry = registry;
+		this.log = log;
+	}
+	
+	@Override
+	public void visit(DStructType structType, RelationRuleBase rr) {
+		if (rr.relInfo.getDatId() != null) {
+			return;
+		}
+		
+		//create new row 
+		//write new schema to db
+		DStructType dtype = registry.getDATType();
+		DValue dval = createDatTableObj(dtype, "dat11");
+		if (dval == null) {
+			return;
+		}
+
+		InsertContext ictx = new InsertContext();
+		DValue newDatIdValue = schemaMigrator.getDbexecutor().executeInsert(dval, ictx);
+		
+		if (newDatIdValue != null) {  
+			rr.relInfo.forceDatId(newDatIdValue.asInt());
+			rr.relInfo.otherSide.forceDatId(newDatIdValue.asInt());
+			String key = createKey(structType.getName(), rr.relInfo.fieldName);
+			log.log("key: %s, created datId: %d", key, newDatIdValue.asInt());
+		}
+	}
+	
+	private DValue createDatTableObj(DStructType type, String datTableName) {
+		StructValueBuilder structBuilder = new StructValueBuilder(type);
+
+		ScalarValueBuilder builder = factorySvc.createScalarValueBuilder(registry);
+		DValue dval = builder.buildString(datTableName);
+		structBuilder.addField("tblName", dval);
+
+		boolean b = structBuilder.finish();
+		if (! b) {
+			return null;
+		}
+		dval = structBuilder.getDValue();
+		return dval;
+	}
+
+	private String createKey(String typeName, String fieldName) {
+		String key = String.format("%s.%s", typeName, fieldName);
+		return key;
+	}
+	
+}
---
+++ src/main/java/org/delia/assoc/PopulateDatIdVisitor.java
@@ -0,0 +1,84 @@
+package org.delia.assoc;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.delia.core.FactoryService;
+import org.delia.db.DBInterface;
+import org.delia.db.schema.FieldInfo;
+import org.delia.db.schema.SchemaMigrator;
+import org.delia.db.schema.SchemaType;
+import org.delia.log.Log;
+import org.delia.rule.rules.RelationRuleBase;
+import org.delia.runner.DoNothingVarEvaluator;
+import org.delia.type.DStructType;
+import org.delia.type.DType;
+import org.delia.type.DTypeRegistry;
+
+public class PopulateDatIdVisitor implements ManyToManyVisitor {
+	private FactoryService factorySvc;
+	private DBInterface dbInterface;
+	private DTypeRegistry registry;
+	private SchemaMigrator schemaMigrator;
+	private Log log;
+	private Map<String,Integer> dataIdMap;
+
+	public PopulateDatIdVisitor(FactoryService factorySvc, DBInterface dbInterface, DTypeRegistry registry, Log log) {
+		this.factorySvc = factorySvc;
+		this.dbInterface = dbInterface;
+		this.registry = registry;
+		this.log = log;
+	}
+	
+	@Override
+	public void visit(DStructType structType, RelationRuleBase rr) {
+		if (rr.relInfo.getDatId() != null) {
+			return;
+		}
+		
+		loadSchemaFingerprintIfNeeded();
+		String key = createKey(structType.getName(), rr.relInfo.fieldName);
+		Integer datId = dataIdMap.get(key);
+		if (datId != null) {  //will be null for new types
+			rr.relInfo.forceDatId(datId);
+			rr.relInfo.otherSide.forceDatId(datId);
+		}
+	}
+
+	private void loadSchemaFingerprintIfNeeded() {
+		//only read from DB if there are MM relations.
+		if (schemaMigrator == null) {
+			schemaMigrator = new SchemaMigrator(factorySvc, dbInterface, registry, new DoNothingVarEvaluator());
+			String fingerprint = schemaMigrator.calcDBFingerprint();
+			log.log("DB fingerprint: " + fingerprint);
+			this.dataIdMap = buildDatIdMap(fingerprint);
+		}
+	}
+	
+	private Map<String,Integer> buildDatIdMap(String fingerprint) {
+		Map<String,Integer> datMap = new HashMap<>();
+		List<SchemaType> list = schemaMigrator.parseFingerprint(fingerprint);
+		for(SchemaType sctype: list) {
+			List<FieldInfo> fieldInfoL = schemaMigrator.parseFields(sctype);
+			for(FieldInfo ff: fieldInfoL) {
+				DType dtype = registry.getType(ff.type);
+				if (dtype != null && dtype.isStructShape()) {
+					String key = createKey(sctype.typeName, ff.name);
+					int datId = ff.datId;
+					datMap.put(key, datId);
+					log.log(String.format("f %s %s", key, ff.type));
+				}
+			}
+		}
+		return datMap;
+	}
+	private String createKey(String typeName, String fieldName) {
+		String key = String.format("%s.%s", typeName, fieldName);
+		return key;
+	}
+
+	public SchemaMigrator getSchemaMigrator() {
+		return schemaMigrator;
+	}
+}
