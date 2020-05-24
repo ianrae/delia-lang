214aaa7e865f28abea64a4c37420fdb2be8bcf22
Ian Rae <ianrae88@gmail.com>
2020-05-24 11:06:29 -0400

    java 8 time

--- src/test/java/org/delia/core/DateFormatServiceTests.java
+++ src/test/java/org/delia/core/DateFormatServiceTests.java
@@ -2,20 +2,106 @@ package org.delia.core;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.DateTimeException;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.Date;
 import java.util.TimeZone;
 
 import org.delia.base.UnitTestLog;
-import org.delia.core.DateFormatService;
-import org.delia.core.DateFormatServiceImpl;
-import org.delia.core.DateFormatter;
-import org.delia.core.TimeZoneService;
-import org.delia.core.TimeZoneServiceImpl;
+import org.delia.error.DeliaError;
 import org.delia.log.Log;
+import org.delia.runner.DeliaException;
 import org.delia.type.WrappedDate;
 import org.junit.Test;
 
 public class DateFormatServiceTests {
+	
+	public static class NewDateFormatServiceImpl implements DateFormatService {
+	    DateTimeFormatter df1 = DateTimeFormatter.ofPattern("yyyy");
+	    DateTimeFormatter df2 = DateTimeFormatter.ofPattern("yyyy-MM");
+	    DateTimeFormatter df3 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	    DateTimeFormatter df4 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH");
+	    DateTimeFormatter df5 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+	    DateTimeFormatter df6 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
+	    DateTimeFormatter df6a = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssVV");
+	    DateTimeFormatter df7 = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS");
+	    DateTimeFormatter dfFull = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSVV");
+		//http://stackoverflow.com/questions/2201925/converting-iso-8601-compliant-string-to-java-util-date
+
+		@Override
+		public Date parse(String input) {
+//			ZoneId zoneId = ZoneId.systemDefault();
+			ZonedDateTime ldt = null;
+			try {
+				DateTimeFormatter formatter = getDateFormat(input);
+				ldt = ZonedDateTime.parse(input, formatter);
+			} catch (DateTimeParseException  e) {
+				DeliaError err = new DeliaError("date-parse-error", e.getMessage());
+				throw new DeliaException(err);
+			}  
+			return java.util.Date.from(ldt.toInstant());
+		}
+
+		private DateTimeFormatter getDateFormat(String input) {
+			int len = input.length();
+			switch(len) {
+			case 4:
+				return df1;
+			case 7:
+				return df2;
+			case 10:
+				return df3;
+			case 13:
+				return df4;
+			case 16:
+				return df5;
+			case 19:
+				return df6;
+			case 23:
+				return df6;
+			case 24:
+				return df6a;
+			default:
+				return dfFull;
+			}
+		}
+
+		@Override
+		public String format(Date dt) {
+			String s = null;
+			try {
+				ZonedDateTime ldt = ZonedDateTime.ofInstant(dt.toInstant(), ZoneId.systemDefault());
+			    s = ldt.format(dfFull);
+			}
+			catch (DateTimeException exc) {
+			    System.out.printf("%s can't be formatted!", dt);
+			    throw exc;
+			}			
+			return s;
+		}
+
+		@Override
+		public TimeZone detectTimezone(String input) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public DateFormatter createFormatter(String input) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public DateFormatter createFormatter() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	}
 	
 	@Test
 	public void test() {
@@ -75,7 +161,8 @@ public class DateFormatServiceTests {
 	TimeZoneService tzSvc = new TimeZoneServiceImpl();
 
 	private DateFormatService createSvc() {
-		return new DateFormatServiceImpl(tzSvc);
+//		return new DateFormatServiceImpl(tzSvc);
+		return new NewDateFormatServiceImpl();
 	}
 
 	protected void log(String s) {
