d266c31a2d783d9a51fd44eb51d87bef81f7395e
Ian Rae <ianrae88@gmail.com>
2020-05-27 12:01:39 -0400

    in [followers] ok

--- src/main/java/org/delia/zdb/mem/MemDBExecutorBase.java
+++ src/main/java/org/delia/zdb/mem/MemDBExecutorBase.java
@@ -5,6 +5,16 @@ import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
+import org.delia.compiler.ast.BooleanExp;
+import org.delia.compiler.ast.Exp;
+import org.delia.compiler.ast.FilterExp;
+import org.delia.compiler.ast.FilterOpFullExp;
+import org.delia.compiler.ast.IdentExp;
+import org.delia.compiler.ast.IntegerExp;
+import org.delia.compiler.ast.QueryExp;
+import org.delia.compiler.ast.QueryFieldExp;
+import org.delia.compiler.ast.QueryFuncExp;
+import org.delia.compiler.ast.QueryInExp;
 import org.delia.core.DateFormatService;
 import org.delia.core.FactoryService;
 import org.delia.core.ServiceBase;
@@ -53,8 +63,65 @@ public abstract class MemDBExecutorBase extends ServiceBase implements ZDBIntern
 		this.fmtSvc = factorySvc.getDateFormatService();
 	}
 
-
 	protected QueryResponse doExecuteQuery(QuerySpec spec, QueryContext qtx) {
+		
+		//avoid infinite loop
+		int maxPreQueries = 10;
+		QuerySpec preQuerySpec = null;
+		QueryResponse qresp0 = null;
+		for(int i = 0; i < maxPreQueries; i++) {
+			preQuerySpec = getPreQuery(spec, preQuerySpec, qresp0);
+			if (preQuerySpec == null) {
+				break;
+			}
+			qresp0 = doSingleQuery(preQuerySpec, qtx);
+		}
+		
+		QueryResponse qresp = doSingleQuery(spec, qtx);
+		return qresp;
+	}
+	//handle things like [55 in [followers]]. need to resolve followers to a list of ids
+	private QuerySpec getPreQuery(QuerySpec spec, QuerySpec preQuerySpec, QueryResponse qresp0) {
+		if (preQuerySpec == null) {
+			if (spec.queryExp.filter != null && spec.queryExp.filter.cond instanceof FilterOpFullExp) {
+				FilterOpFullExp fofe = (FilterOpFullExp) spec.queryExp.filter.cond;
+				if (fofe.isSingleQueryInExp()) {
+					QueryInExp inExp = (QueryInExp) fofe.opexp1;
+					for(Exp exp: inExp.listExp.valueL) {
+						if (exp instanceof IdentExp) {
+							//TODO later support multiple idents in list. we only support one for now.
+							QuerySpec preSpec = new QuerySpec();
+							preSpec.evaluator = spec.evaluator;
+							FilterExp filter = new FilterExp(99, new BooleanExp(true));
+							preSpec.queryExp = new QueryExp(0, new IdentExp(spec.queryExp.typeName), filter, null);
+							QueryFuncExp qfe = new QueryFieldExp(99, new IdentExp(exp.strValue()));
+							preSpec.queryExp.qfelist.add(qfe);
+							return preSpec;
+						}
+					}
+				}
+			}
+		} else {
+			//hack. we assume was the followers thing
+			if (spec.queryExp.filter != null && spec.queryExp.filter.cond instanceof FilterOpFullExp) {
+				FilterOpFullExp fofe = (FilterOpFullExp) spec.queryExp.filter.cond;
+				if (fofe.isSingleQueryInExp()) {
+					QueryInExp inExp = (QueryInExp) fofe.opexp1;
+					inExp.listExp.valueL.clear();
+					for(DValue inner: qresp0.dvalList) {
+						DValue pk = DValueHelper.findPrimaryKeyValue(inner);
+						//TODO fix. create correct Exp type. for now we assume int
+						IntegerExp exp = new IntegerExp(pk.asInt());
+						inExp.listExp.valueL.add(exp);
+					}
+				}
+			}
+		}
+		
+		return null;
+	}
+
+	private QueryResponse doSingleQuery(QuerySpec spec, QueryContext qtx) {
 		QueryResponse qresp = new QueryResponse();
 		RowSelector selector = createSelector(spec); 
 		if (selector == null) {
