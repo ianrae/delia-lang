af615973ba75f69cd07dc8a02064a3cb2472f7a0
Ian Rae <ianrae88@gmail.com>
2021-01-05 18:44:09 -0500

    m

--- src/test/java/org/delia/db/hls/NewHLSTests.java
+++ src/test/java/org/delia/db/hls/NewHLSTests.java
@@ -7,10 +7,15 @@ import java.util.List;
 
 import org.delia.compiler.ast.BooleanExp;
 import org.delia.compiler.ast.Exp;
+import org.delia.compiler.ast.FilterOpExp;
+import org.delia.compiler.ast.FilterOpFullExp;
 import org.delia.compiler.ast.IntegerExp;
 import org.delia.compiler.ast.LongExp;
+import org.delia.compiler.ast.NumberExp;
 import org.delia.compiler.ast.QueryExp;
 import org.delia.compiler.ast.StringExp;
+import org.delia.compiler.astx.XNAFMultiExp;
+import org.delia.compiler.astx.XNAFSingleExp;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,7 +25,7 @@ import org.junit.Test;
  *  
  * type and filter  Customer[id > 10]        initial type and WHERE filter
  *   filter:
- *     true
+ *     true or pkval [15]
  *     [not] val op val
  *     [not] val like str
  *     val in [sdfsdf]
@@ -48,13 +53,13 @@ import org.junit.Test;
  *
  */
 public class NewHLSTests extends HLSTestBase {
-	
+
 	public interface FilterCond {
-		
+
 	}
 	public static class BooleanFilterCond implements FilterCond {
 		private boolean flag;
-		 
+
 		public BooleanFilterCond(boolean flag) {
 			this.flag = flag;
 		}
@@ -64,7 +69,7 @@ public class NewHLSTests extends HLSTestBase {
 	}
 	public static class IntFilterCond implements FilterCond {
 		private int n;
-		 
+
 		public IntFilterCond(int n) {
 			this.n = n;
 		}
@@ -74,7 +79,7 @@ public class NewHLSTests extends HLSTestBase {
 	}
 	public static class LongFilterCond implements FilterCond {
 		private long n;
-		 
+
 		public LongFilterCond(long n) {
 			this.n = n;
 		}
@@ -84,7 +89,7 @@ public class NewHLSTests extends HLSTestBase {
 	}
 	public static class StringFilterCond implements FilterCond {
 		private String str;
-		 
+
 		public StringFilterCond(String str) {
 			this.str = str;
 		}
@@ -109,11 +114,18 @@ public class NewHLSTests extends HLSTestBase {
 		//name,int,boolean,string,fn
 		public ValType valType;
 		public Exp exp;
+		
+		public FilterVal(ValType valType, Exp exp) {
+			this.valType = valType;
+			this.exp = exp;
+		}
+		
 		public boolean asBoolean() {
 			return false;
 		}
 		public int asInt() {
-			return 0; //
+			IntegerExp exp1 = (IntegerExp) exp;
+			return exp1.val.intValue();
 		}
 		public long asLong() {
 			return 0L; //
@@ -124,12 +136,25 @@ public class NewHLSTests extends HLSTestBase {
 		public String asString() {
 			return "";
 		}
+		public String asSymbol() {
+			XNAFSingleExp nafexp = (XNAFSingleExp) exp;
+			return nafexp.funcName;
+		}
 		public FilterFunc asFunc() {
 			return null;
 		}
 	}
 	public static class FilterOp {
 		public String op; //==,!=,<,>,<=,>=
+		
+		public FilterOp(String op) {
+			this.op = op;
+		}
+
+		@Override
+		public String toString() {
+			return op;
+		}
 	}
 	public static class OpFilterCond implements FilterCond {
 		//[not] val op val
@@ -151,9 +176,9 @@ public class NewHLSTests extends HLSTestBase {
 		public boolean isAnd; //if false then is OR
 		public FilterCond val2;
 	}
-	
+
 	public static class FilterCondBuilder {
-		
+
 		public FilterCond build(QueryExp queryExp) {
 			Exp cond = queryExp.filter.cond;
 			if (cond instanceof BooleanExp) {
@@ -168,17 +193,50 @@ public class NewHLSTests extends HLSTestBase {
 			} else if (cond instanceof StringExp) {
 				StringExp exp = (StringExp) queryExp.filter.cond;
 				return new StringFilterCond(exp.val);
+			} else if (cond instanceof FilterOpFullExp) {
+				FilterOpFullExp exp = (FilterOpFullExp) queryExp.filter.cond;
+				if (exp.opexp1 instanceof FilterOpExp) {
+					FilterOpExp foexp = (FilterOpExp) exp.opexp1;
+					if (foexp.op1 instanceof XNAFMultiExp) {
+						XNAFMultiExp xnaf = (XNAFMultiExp) foexp.op1;
+						XNAFSingleExp el = xnaf.qfeL.get(0);
+						
+						OpFilterCond opfiltercond = new OpFilterCond();
+						opfiltercond.isNot = exp.negFlag;
+						opfiltercond.op = new FilterOp(foexp.op);
+						//TODO handle reverse order
+						opfiltercond.val1 = new FilterVal(ValType.SYMBOL, el);
+						opfiltercond.val2 = new FilterVal(createValType(foexp.op2), foexp.op2);
+						return opfiltercond;
+					}
+				}
 			}
 			return null;
 		}
+
+		private ValType createValType(Exp op2) {
+			if (op2 instanceof BooleanExp) {
+				return ValType.BOOLEAN;
+			} else if (op2 instanceof IntegerExp) {
+				return ValType.INT;
+			} else if (op2 instanceof LongExp) {
+				return ValType.LONG;
+			} else if (op2 instanceof NumberExp) {
+				return ValType.NUMBER;
+			} else if (op2 instanceof StringExp) {
+				return ValType.STRING;
+			} else {
+				return null; //TODO: error
+			}
+		}
 	}
-	
-	
+
+
 	@Test
 	public void testBool() {
 		chkbuilderBool("let x = Flight[true]", true);
 		chkbuilderBool("let x = Flight[false]", false);
-	}	
+	}
 	@Test
 	public void testPKInt() {
 		chkbuilderInt("let x = Flight[15]", 15);
@@ -186,18 +244,22 @@ public class NewHLSTests extends HLSTestBase {
 	@Test
 	public void testPKLong() {
 		pkType = "long";
-		chkbuilderLong("let x = Flight[15]", 15L);
+		chkbuilderLong("let x = Flight[2147483648]", 2147483648L);
 	}	
 	@Test
 	public void testPKString() {
 		pkType = "string";
 		chkbuilderString("let x = Flight['abc']", "abc");
 	}	
-	
-	
+
+	@Test
+	public void testOp1() {
+		chkbuilderOpSymbolInt("let x = Flight[field1 < 15]", "field1", "<", 15);
+	}	
+
 	//-------------------------
 	private String pkType = "int";
-	
+
 	@Before
 	public void init() {
 		//createDao();
@@ -230,20 +292,28 @@ public class NewHLSTests extends HLSTestBase {
 		FilterCond cond = builder.build(queryExp);
 		return cond;
 	}
+	private void chkbuilderOpSymbolInt(String src, String val1, String op, int val2) {
+		FilterCond cond = buildCond(src);
+		OpFilterCond ofc = (OpFilterCond) cond;
+		assertEquals(val1, ofc.val1.asSymbol());
+		assertEquals(op, ofc.op.toString());
+		assertEquals(val2, ofc.val2.asInt());
+	}
+
 
 	@Override
 	protected String buildSrc() {
 		String src = String.format("type Flight struct {field1 %s primaryKey, field2 int } end", pkType);
-		
+
 		if (pkType.equals("string")) {
 			src += "\n insert Flight {field1: 'ab', field2: 10}";
 			src += "\n insert Flight {field1: 'cd', field2: 20}";
-			
+
 		} else {
 			src += "\n insert Flight {field1: 1, field2: 10}";
 			src += "\n insert Flight {field1: 2, field2: 20}";
 		}
 		return src;
 	}
-	
+
 }
