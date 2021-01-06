a3ddc66a70e2ba091c9ac4204b624f6ff9b3a998
Ian Rae <ianrae88@gmail.com>
2021-01-05 21:16:11 -0500

    m

--- src/test/java/org/delia/db/hls/NewHLSTests.java
+++ src/test/java/org/delia/db/hls/NewHLSTests.java
@@ -189,6 +189,13 @@ public class NewHLSTests extends HLSTestBase {
 			return null; //TODO!
 		}
 		
+		boolean isSymbol() {
+			return valType.equals(ValType.SYMBOL);
+		}
+		boolean isFn() {
+			return valType.equals(ValType.FUNCTION);
+		}
+		
 		@Override
 		public String toString() {
 			String fn = filterFn == null ? "" : ":" + filterFn.toString();
@@ -470,7 +477,9 @@ public class NewHLSTests extends HLSTestBase {
 
 			return hld;
 		}
-		
+	}
+
+	public static class JoinTreeBuilder {
 		public void generateJoinTree(HLDQuery hld) {
 			for(FetchSpec spec: hld.fetchL) {
 				if (spec.isFK) {
@@ -478,6 +487,64 @@ public class NewHLSTests extends HLSTestBase {
 				} else {
 					addFetch(spec, hld.joinL);
 				}
+			}
+			
+			for(QueryFnSpec fnspec: hld.funcL) {
+				//if parent does .orderBy('addr') then we need a join.
+				if (fnspec.filterFn.fnName.equals("orderBy")) {
+					addOrderBy(fnspec, hld.joinL);
+				}
+			}
+			
+			//TODO: add throughChain
+			
+			if (hld.filter instanceof OpFilterCond) {
+				OpFilterCond ofc = (OpFilterCond) hld.filter;
+				addImplicitJoin(hld.fromType, ofc.val1, hld.joinL);
+				addImplicitJoin(hld.fromType, ofc.val2, hld.joinL);
+			}
+			//TODO: do like and IN filters too, and AndOr
+		}
+		
+		private void addImplicitJoin(DStructType fromType, FilterVal fval, List<JoinElement> resultL) {
+			if (fval.isSymbol()) {
+				String fieldName = fval.asSymbol();
+				addFieldJoinIfNeeded(fromType, fieldName, resultL);
+			} else if (fval.isFn()) {
+				FilterFunc filterFn = fval.asFunc();
+				for(FilterVal inner: filterFn.argL) {
+					addImplicitJoin(fromType, inner, resultL); //*** recursion ***
+				}
+			}
+		}
+
+		private void addFieldJoinIfNeeded(DStructType fromType, String fieldName, List<JoinElement> resultL) {
+			TypePair pair = DRuleHelper.findMatchingStructPair(fromType, fieldName);
+			if (pair != null) {
+				JoinElement el = buildElement(fromType, fieldName, (DStructType) pair.type);
+				switch(el.relinfo.cardinality) {
+				case ONE_TO_ONE:
+				case ONE_TO_MANY:
+					if (el.relinfo.isParent) {
+						addElement(el, resultL);
+					}
+					break;
+				case MANY_TO_MANY:
+					addElement(el, resultL);
+					break;
+				}
+			}
+		}
+
+		private void addOrderBy(QueryFnSpec fnspec, List<JoinElement> resultL) {
+			DStructType structType = fnspec.structField.dtype;
+			String fieldName = fnspec.structField.fieldName;
+			TypePair pair = DRuleHelper.findMatchingPair(structType, fieldName);
+			//ignore sort by scalar fields
+			if (pair != null && pair.type instanceof DStructType) {
+//				addElement(structType, fieldName, (DStructType) pair.type, resultL);
+				JoinElement el = buildElement(structType, fieldName, (DStructType) pair.type);
+				addElement(el, resultL);
 			}
 		}
 		
@@ -525,8 +592,8 @@ public class NewHLSTests extends HLSTestBase {
 
 			resultL.add(el);
 		}
-		
 	}
+	
 	
 	@Test
 	public void testBool() {
@@ -571,6 +638,10 @@ public class NewHLSTests extends HLSTestBase {
 		HLDQuery hld = hldBuilder.build(queryExp);
 		log.log(hld.toString());
 //		assertEquals()
+		
+		JoinTreeBuilder joinBuilder = new JoinTreeBuilder();
+		joinBuilder.generateJoinTree(hld);
+		assertEquals(0, hld.joinL.size());
 	}	
 
 	//-------------------------
