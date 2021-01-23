71d62d07beac15dcdaee1c5059d0e284d083c693
Ian Rae <ianrae88@gmail.com>
2021-01-23 11:05:46 -0500

    remove hls tests

--- src/test/java/org/delia/db/jointree/FieldJoinTreeTests.java
+++ src/test/java/org/delia/db/jointree/FieldJoinTreeTests.java
@@ -1,136 +1,136 @@
-package org.delia.db.jointree;
-
-import org.junit.Before;
-import org.junit.Test;
-
-/**
- * JoinTree extracts all potential JOINS from a query
- * Here are all the types of queries and their joins
- * 
- * C[name]    no join
- * C[addr == 111]  join if C is parent
- * A[cust == 111]  no join (A is child)
- * C[addr == 111 && profile==44]  2 joins
- *  
- * C[addr in(111)]  same as C[addr == 111]
- * C[addr.city == 'toronto'] ""  
- *  
- * C[55].firstName no join
- * C[55].addr   no join since returning Address and its child
- * A[100].cust   join since returning Customer and its parent
- * C[addr == 111].addr  no join. select from address where a.id=111
- * A[cust == 111].cust  no join
- * C[55].addr.city  same as just .addr
- * C[55].addr.country  double join!
- * C[55].orderBy(addr) join since C is parent  
- * A[100].orderBy(cust) no join since A is child  
- * -fetch
- * -fk
- * 
- * @author Ian Rae
- *
- */
-public class FieldJoinTreeTests extends JoinTreeTestBase {
-	
-	@Test
-	public void testPlainFilter() {
-		//1 and 2
-		chkJoinTree("let x = C1[55].addr", "C1|addr|A1"); 
-		chkJoinTree("let x = A1[100].cust", "A1|cust|C1"); 
-		
-		//3 and 4
-		chkJoinTree("let x = CM[55].addr", "CM|addr|AM1"); 
-		chkJoinTree("let x = AM1[100].cust", "AM1|cust|CM");
-		
-		//5 and 6
-		chkJoinTree("let x = CMM[55].addr", "CMM|addr|AMM"); 
-		chkJoinTree("let x = AMM[100].cust", "AMM|cust|CMM"); 
-	}
-	
-	@Test
-	public void testRefFilter() {
-		//1 and 2
-		chkJoinTree("let x = C1[addr < 111].addr", "C1|addr|A1"); 
-		chkJoinTree("let x = A1[cust < 111].cust", "A1|cust|C1");
-		
-		//3 and 4
-		chkJoinTree("let x = CM[addr < 111].addr", "CM|addr|AM1"); 
-		chkJoinTree("let x = AM1[cust < 111].cust", "AM1|cust|CM");
-		
-		//5 and 6
-		chkJoinTree("let x = CMM[addr < 111].addr", "CMM|addr|AMM"); 
-		chkJoinTree("let x = AMM[cust < 111].cust", "AMM|cust|CMM"); 
-	}
-	
+//package org.delia.db.jointree;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+///**
+// * JoinTree extracts all potential JOINS from a query
+// * Here are all the types of queries and their joins
+// * 
+// * C[name]    no join
+// * C[addr == 111]  join if C is parent
+// * A[cust == 111]  no join (A is child)
+// * C[addr == 111 && profile==44]  2 joins
+// *  
+// * C[addr in(111)]  same as C[addr == 111]
+// * C[addr.city == 'toronto'] ""  
+// *  
+// * C[55].firstName no join
+// * C[55].addr   no join since returning Address and its child
+// * A[100].cust   join since returning Customer and its parent
+// * C[addr == 111].addr  no join. select from address where a.id=111
+// * A[cust == 111].cust  no join
+// * C[55].addr.city  same as just .addr
+// * C[55].addr.country  double join!
+// * C[55].orderBy(addr) join since C is parent  
+// * A[100].orderBy(cust) no join since A is child  
+// * -fetch
+// * -fk
+// * 
+// * @author Ian Rae
+// *
+// */
+//public class FieldJoinTreeTests extends JoinTreeTestBase {
+//	
 //	@Test
-//	public void testDoubleRefFilter() {
+//	public void testPlainFilter() {
 //		//1 and 2
-//		chkJoinTree("let x = C1[addr < 111 && something..]", "C1|addr|A1"); 
-//		chkJoinTree("let x = A1[cust < 111]");
+//		chkJoinTree("let x = C1[55].addr", "C1|addr|A1"); 
+//		chkJoinTree("let x = A1[100].cust", "A1|cust|C1"); 
 //		
 //		//3 and 4
-//		chkJoinTree("let x = CM[addr < 111]", "CM|addr|AM1"); 
-//		chkJoinTree("let x = AM1[cust < 111]");
+//		chkJoinTree("let x = CM[55].addr", "CM|addr|AM1"); 
+//		chkJoinTree("let x = AM1[100].cust", "AM1|cust|CM");
 //		
 //		//5 and 6
-//		chkJoinTree("let x = CMM[addr < 111]", "CMM|addr|AMM"); 
-//		chkJoinTree("let x = AMM[cust < 111]", "AMM|cust|CMM"); 
+//		chkJoinTree("let x = CMM[55].addr", "CMM|addr|AMM"); 
+//		chkJoinTree("let x = AMM[100].cust", "AMM|cust|CMM"); 
 //	}
-	
+//	
 //	@Test
-//	public void testInRefFilter() {
+//	public void testRefFilter() {
 //		//1 and 2
-//		chkJoinTree("let x = C1[addr in [111]]", "C1|addr|A1"); 
-//		chkJoinTree("let x = A1[cust < 111]");
+//		chkJoinTree("let x = C1[addr < 111].addr", "C1|addr|A1"); 
+//		chkJoinTree("let x = A1[cust < 111].cust", "A1|cust|C1");
 //		
 //		//3 and 4
-//		chkJoinTree("let x = CM[addr < 111]", "CM|addr|AM1"); 
-//		chkJoinTree("let x = AM1[cust < 111]");
+//		chkJoinTree("let x = CM[addr < 111].addr", "CM|addr|AM1"); 
+//		chkJoinTree("let x = AM1[cust < 111].cust", "AM1|cust|CM");
 //		
 //		//5 and 6
-//		chkJoinTree("let x = CMM[addr < 111]", "CMM|addr|AMM"); 
-//		chkJoinTree("let x = AMM[cust < 111]", "AMM|cust|CMM"); 
+//		chkJoinTree("let x = CMM[addr < 111].addr", "CMM|addr|AMM"); 
+//		chkJoinTree("let x = AMM[cust < 111].cust", "AMM|cust|CMM"); 
 //	}
-	
-	
-	@Test
-	public void testRefSubFieldFilter() {
-		//1 and 2
-		chkJoinTree("let x = C1[addr.y < 111].addr", "C1|addr|A1"); 
-		chkJoinTree("let x = A1[cust.x < 111].cust", "A1|cust|C1");
-		//assume the rest work
-	}
-	
-	@Test
-	public void testSubField() {
-		//1 and 2
-		chkJoinTree("let x = C1[55].addr.y", "C1|addr|A1"); 
-		chkJoinTree("let x = A1[cust < 111].cust.x", "A1|cust|C1");
-		
-		//3 and 4
-		chkJoinTree("let x = CM[addr < 111].addr.y", "CM|addr|AM1"); 
-		chkJoinTree("let x = AM1[cust < 111].cust.x", "AM1|cust|CM");
-		
-		//5 and 6
-		chkJoinTree("let x = CMM[addr < 111].addr.y", "CMM|addr|AMM"); 
-		chkJoinTree("let x = AMM[cust < 111].cust.x", "AMM|cust|CMM"); 
-	}
-	
-	//TODO test C[true].addr.country double join
-
-	@Test
-	public void testDebugSQL() {
-		chkJoinTree("let x = C1[55].addr", "C1|addr|A1"); 
-		chkJoinTree("let x = C1[addr < 111].addr", "C1|addr|A1"); 
-//		chkJoinTree("let x = AMM[cust < 111]", "AMM|cust|CMM"); 
-
-	}
-
-	//---
-	
-	@Before
-	public void init() {
-		createDao();
-	}
-
-}
+//	
+////	@Test
+////	public void testDoubleRefFilter() {
+////		//1 and 2
+////		chkJoinTree("let x = C1[addr < 111 && something..]", "C1|addr|A1"); 
+////		chkJoinTree("let x = A1[cust < 111]");
+////		
+////		//3 and 4
+////		chkJoinTree("let x = CM[addr < 111]", "CM|addr|AM1"); 
+////		chkJoinTree("let x = AM1[cust < 111]");
+////		
+////		//5 and 6
+////		chkJoinTree("let x = CMM[addr < 111]", "CMM|addr|AMM"); 
+////		chkJoinTree("let x = AMM[cust < 111]", "AMM|cust|CMM"); 
+////	}
+//	
+////	@Test
+////	public void testInRefFilter() {
+////		//1 and 2
+////		chkJoinTree("let x = C1[addr in [111]]", "C1|addr|A1"); 
+////		chkJoinTree("let x = A1[cust < 111]");
+////		
+////		//3 and 4
+////		chkJoinTree("let x = CM[addr < 111]", "CM|addr|AM1"); 
+////		chkJoinTree("let x = AM1[cust < 111]");
+////		
+////		//5 and 6
+////		chkJoinTree("let x = CMM[addr < 111]", "CMM|addr|AMM"); 
+////		chkJoinTree("let x = AMM[cust < 111]", "AMM|cust|CMM"); 
+////	}
+//	
+//	
+//	@Test
+//	public void testRefSubFieldFilter() {
+//		//1 and 2
+//		chkJoinTree("let x = C1[addr.y < 111].addr", "C1|addr|A1"); 
+//		chkJoinTree("let x = A1[cust.x < 111].cust", "A1|cust|C1");
+//		//assume the rest work
+//	}
+//	
+//	@Test
+//	public void testSubField() {
+//		//1 and 2
+//		chkJoinTree("let x = C1[55].addr.y", "C1|addr|A1"); 
+//		chkJoinTree("let x = A1[cust < 111].cust.x", "A1|cust|C1");
+//		
+//		//3 and 4
+//		chkJoinTree("let x = CM[addr < 111].addr.y", "CM|addr|AM1"); 
+//		chkJoinTree("let x = AM1[cust < 111].cust.x", "AM1|cust|CM");
+//		
+//		//5 and 6
+//		chkJoinTree("let x = CMM[addr < 111].addr.y", "CMM|addr|AMM"); 
+//		chkJoinTree("let x = AMM[cust < 111].cust.x", "AMM|cust|CMM"); 
+//	}
+//	
+//	//TODO test C[true].addr.country double join
+//
+//	@Test
+//	public void testDebugSQL() {
+//		chkJoinTree("let x = C1[55].addr", "C1|addr|A1"); 
+//		chkJoinTree("let x = C1[addr < 111].addr", "C1|addr|A1"); 
+////		chkJoinTree("let x = AMM[cust < 111]", "AMM|cust|CMM"); 
+//
+//	}
+//
+//	//---
+//	
+//	@Before
+//	public void init() {
+//		createDao();
+//	}
+//
+//}
--- src/test/java/org/delia/db/jointree/SQLFetchTests.java
+++ src/test/java/org/delia/db/jointree/SQLFetchTests.java
@@ -1,95 +1,95 @@
-package org.delia.db.jointree;
-
-import static org.junit.Assert.*;
-
-import java.util.List;
-
-import org.delia.db.hls.HLSSQLGeneratorImpl;
-import org.delia.db.hls.join.JTElement;
-import org.junit.Before;
-import org.junit.Test;
-
-/**
- * @author Ian Rae
- *
- */
-public class SQLFetchTests extends JoinTreeTestBase {
-	
-	@Test
-	public void testPlainFilter() {
-		//1 and 2
-		sqlchkP("let x = C1[55].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM C1 as a LEFT JOIN A1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
-		sqlchkP("let x = A1[100].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM A1 as a LEFT JOIN C1 as b ON a.cust=b.cid WHERE a.id = ?", "100"); 
-		
-//		//3 and 4
-		sqlchkP("let x = CM[55].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM CM as a LEFT JOIN AM1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
-		sqlchkP("let x = AM1[100].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM AM1 as a LEFT JOIN CM as b ON a.cust=b.cid WHERE a.id = ?", "100");
-		
-		//5 and 6
-		sqlchkP("let x = CMM[55].fetch('addr')", "SELECT a.cid,a.x,c.rightv as addr,b.id as addr,b.y FROM CMM as a LEFT JOIN CMMAMMDat1 as c ON a.cid=c.leftv LEFT JOIN AMM as b ON b.id=c.rightv WHERE a.cid = ?", "55"); 
-		sqlchkP("let x = AMM[100].fetch('cust')", "SELECT a.id,a.y,c.leftv as cust,b.cid as cust,b.x FROM AMM as a LEFT JOIN CMMAMMDat1 as c ON a.id=c.rightv LEFT JOIN CMM as b ON b.cid=c.leftv WHERE a.id = ?", "100"); 
-	}
-
-//	//TODO not yet supported
+//package org.delia.db.jointree;
+//
+//import static org.junit.Assert.*;
+//
+//import java.util.List;
+//
+//import org.delia.db.hls.HLSSQLGeneratorImpl;
+//import org.delia.db.hls.join.JTElement;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+///**
+// * @author Ian Rae
+// *
+// */
+//public class SQLFetchTests extends JoinTreeTestBase {
+//	
 //	@Test
-//	public void testPlainFilterFK() {
+//	public void testPlainFilter() {
 //		//1 and 2
-//		chkJoinTree("let x = C1[55].fk('addr')", "C1|addr|A1"); 
-//		chkJoinTree("let x = A1[100].fk('cust')", "A1|cust|C1"); 
+//		sqlchkP("let x = C1[55].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM C1 as a LEFT JOIN A1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
+//		sqlchkP("let x = A1[100].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM A1 as a LEFT JOIN C1 as b ON a.cust=b.cid WHERE a.id = ?", "100"); 
+//		
+////		//3 and 4
+//		sqlchkP("let x = CM[55].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM CM as a LEFT JOIN AM1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
+//		sqlchkP("let x = AM1[100].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM AM1 as a LEFT JOIN CM as b ON a.cust=b.cid WHERE a.id = ?", "100");
+//		
+//		//5 and 6
+//		sqlchkP("let x = CMM[55].fetch('addr')", "SELECT a.cid,a.x,c.rightv as addr,b.id as addr,b.y FROM CMM as a LEFT JOIN CMMAMMDat1 as c ON a.cid=c.leftv LEFT JOIN AMM as b ON b.id=c.rightv WHERE a.cid = ?", "55"); 
+//		sqlchkP("let x = AMM[100].fetch('cust')", "SELECT a.id,a.y,c.leftv as cust,b.cid as cust,b.x FROM AMM as a LEFT JOIN CMMAMMDat1 as c ON a.id=c.rightv LEFT JOIN CMM as b ON b.cid=c.leftv WHERE a.id = ?", "100"); 
+//	}
+//
+////	//TODO not yet supported
+////	@Test
+////	public void testPlainFilterFK() {
+////		//1 and 2
+////		chkJoinTree("let x = C1[55].fk('addr')", "C1|addr|A1"); 
+////		chkJoinTree("let x = A1[100].fk('cust')", "A1|cust|C1"); 
+////		
+////		//3 and 4
+////		chkJoinTree("let x = CM[55].fk('addr')", "CM|addr|AM1"); 
+////		chkJoinTree("let x = AM1[100].fk('cust')", "AM1|cust|CM");
+////		
+////		//5 and 6
+////		chkJoinTree("let x = CMM[55].fk('addr')", "CMM|addr|AMM"); 
+////		chkJoinTree("let x = AMM[100].fk('cust')", "AMM|cust|CMM"); 
+////	}
+//	
+//	@Test
+//	public void testPlainFilterFKs() {
+//		//1 and 2
+//		sqlchkP("let x = C1[55].fks()", "SELECT a.cid,a.x,b.id as addr FROM C1 as a LEFT JOIN A1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
+//		sqlchkP("let x = A1[100].fks()", "SELECT * FROM A1 as a WHERE a.id = ?", "100"); 
 //		
 //		//3 and 4
-//		chkJoinTree("let x = CM[55].fk('addr')", "CM|addr|AM1"); 
-//		chkJoinTree("let x = AM1[100].fk('cust')", "AM1|cust|CM");
+//		sqlchkP("let x = CM[55].fks()", "SELECT a.cid,a.x,b.id as addr FROM CM as a LEFT JOIN AM1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
+//		sqlchkP("let x = AM1[100].fks()", "SELECT * FROM AM1 as a WHERE a.id = ?", "100");
 //		
 //		//5 and 6
-//		chkJoinTree("let x = CMM[55].fk('addr')", "CMM|addr|AMM"); 
-//		chkJoinTree("let x = AMM[100].fk('cust')", "AMM|cust|CMM"); 
+//		sqlchkP("let x = CMM[55].fks()", "SELECT a.cid,a.x,c.rightv as addr FROM CMM as a LEFT JOIN CMMAMMDat1 as c ON a.cid=c.leftv WHERE a.cid = ?", "55"); 
+//		sqlchkP("let x = AMM[100].fks()", "SELECT a.id,a.y,c.leftv as cust FROM AMM as a LEFT JOIN CMMAMMDat1 as c ON a.id=c.rightv WHERE a.id = ?", "100"); 
 //	}
-	
-	@Test
-	public void testPlainFilterFKs() {
-		//1 and 2
-		sqlchkP("let x = C1[55].fks()", "SELECT a.cid,a.x,b.id as addr FROM C1 as a LEFT JOIN A1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
-		sqlchkP("let x = A1[100].fks()", "SELECT * FROM A1 as a WHERE a.id = ?", "100"); 
-		
-		//3 and 4
-		sqlchkP("let x = CM[55].fks()", "SELECT a.cid,a.x,b.id as addr FROM CM as a LEFT JOIN AM1 as b ON a.cid=b.cust WHERE a.cid = ?", "55"); 
-		sqlchkP("let x = AM1[100].fks()", "SELECT * FROM AM1 as a WHERE a.id = ?", "100");
-		
-		//5 and 6
-		sqlchkP("let x = CMM[55].fks()", "SELECT a.cid,a.x,c.rightv as addr FROM CMM as a LEFT JOIN CMMAMMDat1 as c ON a.cid=c.leftv WHERE a.cid = ?", "55"); 
-		sqlchkP("let x = AMM[100].fks()", "SELECT a.id,a.y,c.leftv as cust FROM AMM as a LEFT JOIN CMMAMMDat1 as c ON a.id=c.rightv WHERE a.id = ?", "100"); 
-	}
-	
-	@Test
-	public void testRefFilter() {
-		//1 and 2
-		sqlchkP("let x = C1[addr < 111].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM C1 as a LEFT JOIN A1 as b ON a.cid=b.cust WHERE b.id < ?", "111"); 
-		sqlchkP("let x = A1[cust < 111].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM A1 as a LEFT JOIN C1 as b ON a.cust=b.cid WHERE a.cust < ?", "111");
-		
-		//3 and 4
-		sqlchkP("let x = CM[addr < 111].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM CM as a LEFT JOIN AM1 as b ON a.cid=b.cust WHERE b.id < ?", "111"); 
-		sqlchkP("let x = AM1[cust < 111].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM AM1 as a LEFT JOIN CM as b ON a.cust=b.cid WHERE a.cust < ?", "111");
-		
-//		//5 and 6
-		sqlchkP("let x = CMM[addr < 111].fetch('addr')", "SELECT a.cid,a.x,c.rightv as addr,b.id as addr,b.y FROM CMM as a LEFT JOIN CMMAMMDat1 as c ON a.cid=c.leftv LEFT JOIN AMM as b ON b.id=c.rightv WHERE c.rightv < ?", "111"); 
-		sqlchkP("let x = AMM[cust < 111].fetch('cust')", "SELECT a.id,a.y,c.leftv as cust,b.cid as cust,b.x FROM AMM as a LEFT JOIN CMMAMMDat1 as c ON a.id=c.rightv LEFT JOIN CMM as b ON b.cid=c.leftv WHERE c.leftv < ?", "111"); 
-	}
-	
-
-	@Test
-	public void testDebugSQL() {
-	}
-
-	//---
-	
-	@Before
-	public void init() {
-		createDao();
-//		HLSSQLGeneratorImpl.useJoinTreeFlag = true;		
-	}
-	private void sqlchkP(String src, String sqlExpected, String param1) {
-		doSqlchkP(src, sqlExpected, param1);
-	}
-
-}
+//	
+//	@Test
+//	public void testRefFilter() {
+//		//1 and 2
+//		sqlchkP("let x = C1[addr < 111].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM C1 as a LEFT JOIN A1 as b ON a.cid=b.cust WHERE b.id < ?", "111"); 
+//		sqlchkP("let x = A1[cust < 111].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM A1 as a LEFT JOIN C1 as b ON a.cust=b.cid WHERE a.cust < ?", "111");
+//		
+//		//3 and 4
+//		sqlchkP("let x = CM[addr < 111].fetch('addr')", "SELECT a.cid,a.x,b.id as addr,b.y,b.cust FROM CM as a LEFT JOIN AM1 as b ON a.cid=b.cust WHERE b.id < ?", "111"); 
+//		sqlchkP("let x = AM1[cust < 111].fetch('cust')", "SELECT a.id,a.y,a.cust,b.cid as cust,b.x FROM AM1 as a LEFT JOIN CM as b ON a.cust=b.cid WHERE a.cust < ?", "111");
+//		
+////		//5 and 6
+//		sqlchkP("let x = CMM[addr < 111].fetch('addr')", "SELECT a.cid,a.x,c.rightv as addr,b.id as addr,b.y FROM CMM as a LEFT JOIN CMMAMMDat1 as c ON a.cid=c.leftv LEFT JOIN AMM as b ON b.id=c.rightv WHERE c.rightv < ?", "111"); 
+//		sqlchkP("let x = AMM[cust < 111].fetch('cust')", "SELECT a.id,a.y,c.leftv as cust,b.cid as cust,b.x FROM AMM as a LEFT JOIN CMMAMMDat1 as c ON a.id=c.rightv LEFT JOIN CMM as b ON b.cid=c.leftv WHERE c.leftv < ?", "111"); 
+//	}
+//	
+//
+//	@Test
+//	public void testDebugSQL() {
+//	}
+//
+//	//---
+//	
+//	@Before
+//	public void init() {
+//		createDao();
+////		HLSSQLGeneratorImpl.useJoinTreeFlag = true;		
+//	}
+//	private void sqlchkP(String src, String sqlExpected, String param1) {
+//		doSqlchkP(src, sqlExpected, param1);
+//	}
+//
+//}
