ba6bae8c94f9897e307c26bc108b96130f96f130
Ian Rae <ianrae88@gmail.com>
2021-01-26 06:49:02 -0500

    omg

--- src/main/java/org/delia/db/postgres/PostgresSqlMergeAllIntoStatement.java
+++ src/main/java/org/delia/db/postgres/PostgresSqlMergeAllIntoStatement.java
@@ -13,6 +13,8 @@ import org.delia.db.sqlgen.SqlValueListClause;
 import org.delia.hld.HLDField;
 import org.delia.hld.cud.HLDUpdate;
 import org.delia.type.DValue;
+import org.delia.type.TypePair;
+import org.delia.util.DValueHelper;
 
 public class PostgresSqlMergeAllIntoStatement extends SqlMergeAllIntoStatement {
 
@@ -32,46 +34,60 @@ public class PostgresSqlMergeAllIntoStatement extends SqlMergeAllIntoStatement {
 	
 	@Override
 	public SqlStatement render() {
-//INSERT INTO customers (name, email)
-//		VALUES('Microsoft','hotline@microsoft.com') 
-//		ON CONFLICT (name) 
-//		DO 
-//		   UPDATE SET email = EXCLUDED.email || ';' || customers.email;
-		
-		
 		SqlStatement stm = new SqlStatement(hld);
 		StrCreator sc = new StrCreator();
 		sc.o("INSERT INTO");
 		sc.o(tblClause.render(stm));
 		
-		DValue first = valueClause.getValueL().remove(0);
-		valueClause.getValueL().clear();
-		valueClause.getValueL().add(first);
-		sc.o(valueClause.render(stm));
-
-		sc.o(" ON CONFLICT");
 		sc.o("(%s,%s)", hld.mergeKey,hld.mergeKeyOther);
-		sc.o(" DO UPDATE");
-		sc.o(" SET ");
-		int index = 0;
 		
-		List<HLDField> oneFieldL = hld.fieldL.stream().filter(x -> x.fieldName.equals(hld.mergeKeyOther)).collect(Collectors.toList());
-		ListWalker<HLDField> walker = new ListWalker<>(oneFieldL);
-		String conditionStr = null;
-		while(walker.hasNext()) {
-			HLDField ff = walker.next();
-			DValue inner = hld.valueL.get(index);
-			stm.paramL.add(valueClause.renderValue(inner));
-			
-			conditionStr = String.format("%s = %s", ff.render(), "?");
-			sc.o(conditionStr);
-			walker.addIfNotLast(sc, ", ");
-			index++;
-		}
-
+		DValue first = valueClause.getValueL().remove(0);
+		stm.paramL.add(valueClause.renderValue(first));
+		String tblName = hld.assocRelInfo.nearType.getName();
+		TypePair pkpair = DValueHelper.findPrimaryKeyFieldPair(hld.assocRelInfo.nearType);
+		sc.o(" SELECT %s,? from %s", pkpair.name, tblName);
 		
 		stm.sql = sc.toString();
 		return stm;
 	}
+
+//	@Override  OLD
+//	public SqlStatement render() {
+//		SqlStatement stm = new SqlStatement(hld);
+//		StrCreator sc = new StrCreator();
+//		sc.o("INSERT INTO");
+//		sc.o(tblClause.render(stm));
+//		
+//		DValue first = valueClause.getValueL().remove(0);
+//		valueClause.getValueL().clear();
+//		valueClause.getValueL().add(first);
+//		sc.o(valueClause.render(stm));
+//
+//		sc.o(" ON CONFLICT");
+//		sc.o("(%s,%s)", hld.mergeKey,hld.mergeKeyOther);
+//		sc.o(" DO UPDATE");
+//		sc.o(" SET ");
+//		int index = 0;
+//		
+//		List<HLDField> oneFieldL = hld.fieldL.stream().filter(x -> x.fieldName.equals(hld.mergeKeyOther)).collect(Collectors.toList());
+//		ListWalker<HLDField> walker = new ListWalker<>(oneFieldL);
+//		String conditionStr = null;
+//		while(walker.hasNext()) {
+//			HLDField ff = walker.next();
+//			DValue inner = hld.valueL.get(index);
+//			stm.paramL.add(valueClause.renderValue(inner));
+//			
+//			conditionStr = String.format("%s = %s", ff.render(), "?");
+//			sc.o(conditionStr);
+//			walker.addIfNotLast(sc, ", ");
+//			index++;
+//		}
+//
+//		
+//		stm.sql = sc.toString();
+//		return stm;
+//	}
+	
+	
 	
 }
--- src/test/java/org/delia/postgres/PostgresBDDTests.java
+++ src/test/java/org/delia/postgres/PostgresBDDTests.java
@@ -321,7 +321,7 @@ public class PostgresBDDTests extends BDDBase {
 	
 	@Test
 	public void testDebug() {
-		testIndexToRun = 2;
+//		testIndexToRun = 2;
 		//BDDTesterEx.disableSQLLoggingDuringSchemaMigration = false;
 		enableAllFileCheck = false;
 		enableSQLLogging = true;
