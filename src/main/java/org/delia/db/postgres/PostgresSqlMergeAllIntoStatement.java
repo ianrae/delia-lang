a81fc1744012007a295b69f54f6f191d4c698455
Ian Rae <ianrae88@gmail.com>
2021-01-25 20:28:00 -0500

    almost

--- src/main/java/org/delia/db/postgres/PostgresSqlMergeAllIntoStatement.java
+++ src/main/java/org/delia/db/postgres/PostgresSqlMergeAllIntoStatement.java
@@ -1,14 +1,12 @@
 package org.delia.db.postgres;
 
-import java.util.List;
-import java.util.Optional;
-
 import org.delia.db.SqlStatement;
 import org.delia.db.sql.StrCreator;
+import org.delia.db.sql.table.ListWalker;
 import org.delia.db.sqlgen.SqlMergeAllIntoStatement;
 import org.delia.db.sqlgen.SqlTableNameClause;
 import org.delia.db.sqlgen.SqlValueListClause;
-import org.delia.hld.SqlColumn;
+import org.delia.hld.HLDField;
 import org.delia.type.DValue;
 
 public class PostgresSqlMergeAllIntoStatement extends SqlMergeAllIntoStatement {
@@ -19,39 +17,39 @@ public class PostgresSqlMergeAllIntoStatement extends SqlMergeAllIntoStatement {
 
 	@Override
 	public SqlStatement render() {
+//INSERT INTO customers (name, email)
+//		VALUES('Microsoft','hotline@microsoft.com') 
+//		ON CONFLICT (name) 
+//		DO 
+//		   UPDATE SET email = EXCLUDED.email || ';' || customers.email;
+		
+		
 		SqlStatement stm = new SqlStatement(hld);
 		StrCreator sc = new StrCreator();
-		sc.o("xxxxxxxMERGE INTO");
+		sc.o("INSERT INTO");
 		sc.o(tblClause.render(stm));
 		
-//		sc.o(" KEY(%s)", hld.mergeKey);
-//		sc.o(valueClause.render(stm));
-		
-		String alias = hld.typeOrTbl.alias;
-		SqlColumn col = new SqlColumn(alias, hld.mergeKey);
-		
-		//USING (SELECT id FROM CUSTOMER) AS S ON T.leftv = s.id
-		sc.o(" USING (SELECT %s FROM %s) AS S", hld.mergePKField, hld.mergeType);
-		sc.o(" ON %s = s.%s", col.render(), hld.mergePKField);
-		
-		//WHEN MATCHED THEN UPDATE SET T.rightv = ?
-		col = new SqlColumn(alias, hld.mergeKeyOther);
-		sc.o(" WHEN MATCHED THEN UPDATE SET %s = ?", col.render());
-		DValue inner = findFirstNonNullValue(hld.valueL); 
-		stm.paramL.add(inner);
-//		stm.paramL.add(inner);
-		
-//		//WHEN NOT MATCHED THEN INSERT (leftv, rightv) VALUES(s.id, ?)
-//		sc.o(" WHEN NOT MATCHED THEN INSERT (leftv, rightv)", hld.mergeKey, hld.mergeKeyOther);
-//		sc.o(" VALUES(s.%s, ?)", hld.mergePKField);
+		sc.o(valueClause.render(stm));
+
+		sc.o(" ON CONFLICT DO UPDATE");
+		sc.o(" SET ");
+		int index = 0;
+		ListWalker<HLDField> walker = new ListWalker<>(hld.fieldL);
+		String conditionStr = null;
+		while(walker.hasNext()) {
+			HLDField ff = walker.next();
+			DValue inner = hld.valueL.get(index);
+			stm.paramL.add(valueClause.renderValue(inner));
+			
+			conditionStr = String.format("%s = %s", ff.render(), "?");
+			sc.o(conditionStr);
+			walker.addIfNotLast(sc, ", ");
+			index++;
+		}
+
 		
 		stm.sql = sc.toString();
 		return stm;
 	}
 	
-	private DValue findFirstNonNullValue(List<DValue> valueL) {
-		Optional<DValue> opt = valueL.stream().filter(x -> x != null).findFirst();
-		return opt.orElse(null);
-	}
-
 }
