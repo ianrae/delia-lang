aed21622556091ee5edf557d569ab7e08593e3db
Ian Rae <ianrae88@gmail.com>
2021-02-07 10:14:22 -0500

    getter

---
+++ src/main/java/org/delia/codegen/CodeGenHelper.java
@@ -0,0 +1,179 @@
+package org.delia.codegen;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.delia.db.sql.StrCreator;
+import org.delia.type.DStructType;
+import org.delia.type.DType;
+import org.delia.type.DTypeRegistry;
+import org.delia.type.Shape;
+import org.delia.type.TypePair;
+import org.delia.util.DValueHelper;
+import org.delia.util.StringUtil;
+
+public class CodeGenHelper {
+	public DTypeRegistry registry;
+	public String packageName;
+
+	public CodeGenHelper(DTypeRegistry registry, String packageName) {
+		this.registry = registry;
+		this.packageName = packageName;
+	}
+	public String convertToJava(DType ftype) {
+		switch(ftype.getShape()) {
+		case INTEGER:
+			return "Integer";
+		case LONG:
+			return "Long";
+		case NUMBER:
+			return "Double";
+		case BOOLEAN:
+			return "Boolean";
+		case STRING:
+			return "String";
+		case DATE:
+			return "ZonedDateTime";
+		case STRUCT:
+		{
+			return ftype.getName();
+		}
+		default:
+			return null;
+		}
+	}
+
+	public String convertToJava(DStructType structType, String fieldName) {
+		boolean flag = !structType.fieldIsOptional(fieldName);
+		DType ftype = structType.getDeclaredFields().get(fieldName);
+		return convertToJava(structType, fieldName, ftype, flag);
+	}
+	public String convertToJava(DStructType structType, String fieldName, DType ftype, boolean flag) {
+		switch(ftype.getShape()) {
+		case INTEGER:
+			return flag ? "int" : "Integer";
+		case LONG:
+			return flag ? "long": "Long";
+		case NUMBER:
+			return flag ? "double" : "Double";
+		case BOOLEAN:
+			return flag ? "boolean" : "Boolean";
+		case STRING:
+			return "String";
+		case DATE:
+			return "ZonedDateTime";
+		case STRUCT:
+		{
+			return ftype.getName();
+		}
+		default:
+			return null;
+		}
+	}
+
+	public String convertToAsFn(DType ftype) {
+		switch(ftype.getShape()) {
+		case INTEGER:
+			return "asInt";
+		case LONG:
+			return "asLong";
+		case NUMBER:
+			return "asNumber";
+		case BOOLEAN:
+			return "asBoolean";
+		case STRING:
+			return "asString";
+		case DATE:
+			return "asDate";
+		case STRUCT:
+		{
+			return "TODOfix" + ftype.getName();
+		}
+		default:
+			return null;
+		}
+	}
+	public String getNullValueFor(DStructType structType, String fieldName) {
+		boolean flag = !structType.fieldIsOptional(fieldName);
+		DType ftype = structType.getDeclaredFields().get(fieldName);
+		switch(ftype.getShape()) {
+		case INTEGER:
+			return flag ? "0" : "null";
+		case LONG:
+			return flag ? "0": "null";
+		case NUMBER:
+			return flag ? "0.0" : "null";
+		case BOOLEAN:
+			return flag ? "false" : "null";
+		case STRING:
+			return "null";
+		case DATE:
+			return "null";
+		case STRUCT:
+		{
+			return "null";
+		}
+		default:
+			return null;
+		}
+	}
+
+	public List<String> getImportList(DStructType structType) {
+		List<String> list = new ArrayList<>();
+		Map<String,String> alreadyMap = new HashMap<>();
+		for(String fieldName: structType.getDeclaredFields().keySet()) {
+ 			DType ftype = structType.getDeclaredFields().get(fieldName);
+			if (ftype.isShape(Shape.DATE)) {
+				if (alreadyMap.containsKey("Date")) {
+					continue;
+				}
+				String s = String.format("import java.time.ZonedDateTime;");
+				list.add(s);
+				alreadyMap.put("Date", "");
+			} else if (ftype.isStructShape()) {
+				if (alreadyMap.containsKey(ftype.getName())) {
+					continue;
+				}
+				String s = String.format("import %s.%s;", packageName, ftype.getName());
+				list.add(s);
+				alreadyMap.put(ftype.getName(), "");
+			}
+
+		}
+		return list;
+	}
+
+	public void addImports(StrCreator sc, DStructType structType) {
+		sc.o("package %s;", packageName);
+		sc.nl();
+		for(String s: getImportList(structType)) {
+			sc.o(s);
+			sc.nl();
+		}
+		sc.nl();
+		sc.nl();
+	}
+
+
+	public boolean hasPK(DType ftype) {
+		TypePair pkPair = DValueHelper.findPrimaryKeyFieldPair(ftype);
+		return pkPair != null;
+	}
+
+	public String getPKType(DType ftype) {
+		TypePair pkPair = DValueHelper.findPrimaryKeyFieldPair(ftype);
+		return convertToJava(pkPair.type);
+	}
+	public String getPKField(DType ftype) {
+		TypePair pkPair = DValueHelper.findPrimaryKeyFieldPair(ftype);
+		return StringUtil.uppify(pkPair.name);
+	}
+
+	public String getPKTypeAsFn(DType ftype) {
+		TypePair pkPair = DValueHelper.findPrimaryKeyFieldPair(ftype);
+		return convertToAsFn(pkPair.type);
+	}
+
+}
--- src/test/java/org/delia/codegen/NewCodegenTests.java
+++ src/test/java/org/delia/codegen/NewCodegenTests.java
@@ -7,14 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.delia.api.Delia;
 import org.delia.api.DeliaSession;
 import org.delia.db.sizeof.DeliaTestBase;
-import org.delia.type.BlobType;
+import org.delia.db.sql.StrCreator;
+import org.delia.type.DStructType;
+import org.delia.type.DType;
 import org.delia.type.DTypeRegistry;
-import org.delia.type.DValue;
-import org.delia.type.WrappedBlob;
-import org.delia.util.BlobUtils;
+import org.delia.util.StringUtil;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -25,25 +24,192 @@ import org.junit.Test;
  */
 public class NewCodegenTests extends DeliaTestBase { 
 	
+	public static class CodeGeneratorOptions {
+		public boolean addJsonIgnoreToRelations;
+	}
+	
 	public interface CodeGenerator {
-		
+		 void setOptions(CodeGeneratorOptions options);
+		 void setRegistry(DTypeRegistry registry);
+		 void setPackageName(String packageName);
+	     boolean canProcess(DType dtype);
+	     String buildJavaFileName(DType dtype);
+	     String generate(DType dtype);
 	}
 	
 	public static class CodeGeneratorService {
 
-		public CodeGeneratorService(List<String> typeNames, List<CodeGenerator> generatorsL, String packageName) {
-			// TODO Auto-generated constructor stub
+		private List<String> typeNameL;
+		private List<CodeGenerator> generatorL;
+		private String packageName;
+		private DTypeRegistry registry;
+		private File outputDir;
+		private StringBuilder sb;
+		private CodeGeneratorOptions options = new CodeGeneratorOptions();
+		
+		public CodeGeneratorService(DTypeRegistry registry, List<String> typeNames, List<CodeGenerator> generatorsL, String packageName) {
+			this.registry = registry;
+			this.typeNameL = typeNames;
+			this.generatorL = generatorsL;
+			this.packageName = packageName;
 		}
 
 		public boolean run(File dir) {
-			// TODO Auto-generated method stub
-			return false;
+			this.outputDir = dir;
+			return doRun(true);
 		}
 		public boolean run(StringBuilder sb) {
-			// TODO Auto-generated method stub
-			return false;
+			this.sb = sb;
+			return doRun(false);
 		}
 		
+		protected boolean doRun(boolean outputToFile) {
+			
+			for(String typeName: typeNameL) {
+				DType dtype = registry.getType(typeName);
+				
+				for(CodeGenerator gen: generatorL) {
+					gen.setRegistry(registry);
+					gen.setOptions(options);
+					gen.setPackageName(typeName);
+					if (!gen.canProcess(dtype)) {
+						continue;
+					}
+					
+					String fileName = gen.buildJavaFileName(dtype);
+					String text = gen.generate(dtype);
+					
+					if (outputToFile) {
+						writeToFile(fileName, text);
+					} else {
+						sb.append(text);
+						sb.append(StringUtil.eol());
+					}
+				}
+				
+			}
+			return true;
+		}
+
+		private void writeToFile(String fileName, String text) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public CodeGeneratorOptions getOptions() {
+			return options;
+		}
+
+		public void setOptions(CodeGeneratorOptions options) {
+			this.options = options;
+		}
+	}
+	
+	public static abstract class NewCodeGenBase implements CodeGenerator {
+		
+		protected CodeGeneratorOptions options;
+		protected String packageName;
+		protected boolean structTypesOnly;
+		private DTypeRegistry registry;
+		private CodeGenHelper helper;
+
+		public NewCodeGenBase(boolean structTypesOnly) {
+			this.structTypesOnly = structTypesOnly;
+			this.helper = null;//new CodeGenHelper(DTypeRegistry registry, String packageName) {
+
+		}
+		
+		protected CodeGenHelper helper() {
+			if (this.helper == null) {
+				helper = new CodeGenHelper(registry, packageName);
+			}
+			return helper;
+		}
+
+		@Override
+		public void setOptions(CodeGeneratorOptions options) {
+			this.options = options;
+		}
+
+		@Override
+		public void setPackageName(String packageName) {
+			this.packageName = packageName;
+		}
+
+		@Override
+		public boolean canProcess(DType dtype) {
+			if (structTypesOnly) {
+				return dtype.isStructShape();
+			}
+			return false;
+		}
+
+		@Override
+		public String buildJavaFileName(DType dtype) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public void setRegistry(DTypeRegistry registry) {
+			this.registry = registry;
+		}
+	}
+	
+	public static class GetterInterfaceCodeGen extends NewCodeGenBase {
+
+		public GetterInterfaceCodeGen() {
+			super(true);
+		}
+
+		@Override
+		public String generate(DType typeParam) {
+			DStructType structType = (DStructType) typeParam; //structTypesOnly is true
+			String typeName = structType.getName();
+
+			StrCreator sc = new StrCreator();
+			helper().addImports(sc, structType);
+			sc.o("import org.delia.codegen.DeliaImmutable;");
+			sc.nl();
+			sc.nl();
+
+			String baseType = (structType.getBaseType() == null) ? "DeliaImmutable" : structType.getBaseType().getName();
+			sc.o("public interface %s extends %s {", typeName, baseType);
+			sc.nl();
+			for(String fieldName: structType.getDeclaredFields().keySet()) {
+				DType ftype = structType.getDeclaredFields().get(fieldName);
+
+				String javaType = helper().convertToJava(structType, fieldName);
+				boolean hasPK = helper().hasPK(ftype);
+				if (options.addJsonIgnoreToRelations && hasPK) {
+					sc.o("  @JsonIgnore");
+					sc.nl();
+				}
+				sc.o("  %s get%s();", javaType, StringUtil.uppify(fieldName));
+				sc.nl();
+				
+				if (hasPK) {
+					String pkType = helper().getPKType(ftype);
+					sc.o("  %s get%sPK();", pkType, StringUtil.uppify(fieldName));
+					sc.nl();
+				}
+
+			}
+			sc.o("}");
+			sc.nl();
+
+			return sc.toString();
+		}
+
+
+		protected List<String> getImportList(DStructType structType) {
+			List<String> list = helper().getImportList(structType);
+			if (options.addJsonIgnoreToRelations) {
+				list.add("import com.fasterxml.jackson.annotation.JsonIgnore;");
+			}
+			return list;
+		}
+
 	}
 	
 	public static class CGBuilder4 {
@@ -55,7 +221,8 @@ public class NewCodegenTests extends DeliaTestBase {
 		
 		public CodeGeneratorService build() {
 			List<String> typeNames = buildTypeNamesList();
-			return new CodeGeneratorService(typeNames, builder3.builder2.generatorsL, builder3.packageName);
+			CodegenBuilder builder = builder3.builder2.builder;
+			return new CodeGeneratorService(builder.registry, typeNames, builder3.builder2.generatorsL, builder3.packageName);
 		}
 
 		private List<String> buildTypeNamesList() {
@@ -89,6 +256,8 @@ public class NewCodegenTests extends DeliaTestBase {
 		}
 		
 		public CGBuilder3 addStandardGenerators() {
+			generatorsL.add(new GetterInterfaceCodeGen());
+			//TODO more
 			return new CGBuilder3(this);
 		}
 		public CGBuilder3 addGenerator(CodeGenerator generator) {
@@ -137,7 +306,9 @@ public class NewCodegenTests extends DeliaTestBase {
 		
 		StringBuilder sb = new StringBuilder();
 		boolean b2 = codegen.run(sb);
+		log.log("==== output ====");
 		log.log(sb.toString());
+		assertEquals(true, b2);
 	}	
 	
 	
