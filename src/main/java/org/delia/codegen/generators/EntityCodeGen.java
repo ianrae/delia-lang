1c2037998fe34a962945369b1825826aacc9dbe3
Ian Rae <ianrae88@gmail.com>
2021-02-11 21:10:59 -0500

    m

--- src/main/java/org/delia/codegen/generators/EntityCodeGen.java
+++ src/main/java/org/delia/codegen/generators/EntityCodeGen.java
@@ -56,12 +56,17 @@ public class EntityCodeGen extends CodeGenBase {
 		for(String fieldName: structType.getDeclaredFields().keySet()) {
 			DType ftype = structType.getDeclaredFields().get(fieldName);
 
+			boolean isList = helper().isList(structType, fieldName);
 			String javaType = helper().convertToJava(structType, fieldName);
 			String javaObjType = helper().convertToJava(ftype, false);
 			String asFn = helper().convertToAsFn(ftype);
 			String nullValue = helper().getNullValueFor(structType, fieldName);
 
 			if (ftype.isStructShape()) {
+				if (isList) {
+					doList(g, sc, javaType, javaObjType, fieldName, ftype);
+					continue;
+				}
 				//t4(ftype,fobjname,uname,fname) ::= <<
 				st = g.getInstanceOf("t4");
 				st.add("ftype", javaType);
@@ -100,5 +105,32 @@ public class EntityCodeGen extends CodeGenBase {
 		sc.nl();
 
 		return sc.toString();
+	}
+
+	private void doList(STGroup g, StrCreator sc, String javaType, String javaObjType, String fieldName, DType ftype) {
+		//t4(ftype,fobjname,uname,fname) ::= <<
+		ST st = g.getInstanceOf("t4list");
+		st.add("ftype", javaType);
+		st.add("listftype", String.format("List<%s>", javaType));
+		st.add("fobjname", javaObjType);
+		st.add("uname", StringUtil.uppify(fieldName));
+		st.add("fname", fieldName);
+		st.add("nullval", "null");
+		sc.addStr(st.render());
+		
+		if (helper().hasPK(ftype)) {
+			//t5(ftype,uname,pktype,pkfield) ::= <<
+			st = g.getInstanceOf("t5list");
+			st.add("ftype", javaType);
+			st.add("listftype", String.format("List<%s>", javaType));
+			st.add("uname", StringUtil.uppify(fieldName));
+			st.add("uname2", ftype.getName());
+			st.add("pktype", helper().getPKType(ftype));
+			st.add("listpktype", String.format("List<%s>", helper().getPKType(ftype)));
+			st.add("pkfield", helper().getPKField(ftype));
+			sc.addStr(st.render());
+		}
+		sc.nl();
+		
 	}
 }
