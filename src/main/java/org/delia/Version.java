86eff92870eaa747cf76c4cd54813bfd7b63e33c
Ian Rae <contractor_irae@t-insight.com>
2021-06-23 18:37:59 -0400

    version

--- src/main/java/org/delia/Version.java
+++ src/main/java/org/delia/Version.java
@@ -1,8 +1,9 @@
 package org.delia;
 
 public class Version {
-	private static final String MAIN_VERSION = "0.3.1";
-	public static final String VERSION = MAIN_VERSION + ".1"; //increment this each time
-	
+	private static final String MAIN_VERSION = "0.3.2";
+	public static final String VERSION = MAIN_VERSION + ".0"; //increment this each time
+
+	//23Jun2021: add seed syntax (eg.exists)
 	//9feb2021: added DataSource support
 }
