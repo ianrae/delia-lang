604787625381f80bbe6722b733d8f818789ea09f
Ian Rae <contractor_irae@t-insight.com>
2023-09-30 09:58:22 -0400

    v0.4.5

--- src/main/java/org/delia/Version.java
+++ src/main/java/org/delia/Version.java
@@ -2,8 +2,9 @@ package org.delia;
 
 public class Version {
 
-    private static final String VERSION = "0.4.4";
+    private static final String VERSION = "0.4.5";
 
+    //30Sep2023: v0.4.4 fix one-way relation to not be UNIQUE
     //10Sep2023: v0.4.4 add sqlTableName and fix thread issue with MEM
     //23Jan2023: v0.4.3 fix childSession issue
     //10Jan2023: v0.4.2 merge delia-antlr. BIG CHANGE
