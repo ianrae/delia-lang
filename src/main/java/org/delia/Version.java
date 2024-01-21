40f01deab84df4725b7287debcf55a46b7638d75
Ian Rae <contractor_irae@t-insight.com>
2024-01-21 09:08:44 -0500

    v0.4.6

--- src/main/java/org/delia/Version.java
+++ src/main/java/org/delia/Version.java
@@ -2,9 +2,10 @@ package org.delia;
 
 public class Version {
 
-    private static final String VERSION = "0.4.5";
+    private static final String VERSION = "0.4.6";
 
-    //30Sep2023: v0.4.4 fix one-way relation to not be UNIQUE
+    //21Jan2024: v0.4.6 fix bug when 2 possible primary keys
+    //30Sep2023: v0.4.5 fix one-way relation to not be UNIQUE
     //10Sep2023: v0.4.4 add sqlTableName and fix thread issue with MEM
     //23Jan2023: v0.4.3 fix childSession issue
     //10Jan2023: v0.4.2 merge delia-antlr. BIG CHANGE
